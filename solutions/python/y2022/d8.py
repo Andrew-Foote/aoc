9b1316cb7c667610b3478327a52bcc2a84066102
Andrew Foote <footeandrew1@gmail.com>
2022-12-08 12:58:34 +0000

    d8p2

--- solutions/python/y2022/d8.py
+++ solutions/python/y2022/d8.py
@@ -5,7 +5,8 @@ test_inputs = [('example', '''\
 33549
 35390\
 ''', [
-	('p1', '21')
+	('p1', '21'),
+	('p2', '8')
 ])]
 
 def p1(ip: str) -> int:
@@ -40,3 +41,43 @@ def p1(ip: str) -> int:
 		return False
 
 	return sum(1 for i, j in trees if is_visible(i, j))
+
+def p2(ip: str) -> int:
+	grid = [list(map(int, s)) for s in ip.splitlines()]
+	h = len(grid)
+	w = len(grid[0])
+	assert all(len(row) == w for row in grid[1:])
+	trees = [(i, j) for i in range(h) for j in range(w)]
+
+	def score(i: int, j: int) -> bool:
+		treeheight = grid[i][j]
+		dists = {}
+
+		for d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+			new_i = i
+			new_j = j
+			k = 0
+
+			while True:
+				k += 1
+				new_i = i + d[0] * k
+				new_j = j + d[1] * k
+				#if (i, j) == (3, 2): print(d, k, new_i, new_j)
+				
+				if (
+					new_i < 0 or new_j < 0 
+					or new_i >= h or new_j >= w
+				):
+					# visible in this dir
+					dists[d] = k - 1
+					break
+
+				if grid[new_i][new_j] >= treeheight:
+					# not visible in this dir
+					dists[d] = k
+					break
+
+		#print(i, j, dists)
+		return dists[(0, 1)] * dists[(0, -1)] * dists[(1, 0)] * dists[(-1, 0)]
+
+	return max(score(i, j) for i, j in trees)
