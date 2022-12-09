9250d12af988555d729c5e2a1b86514f58a78f49
Andrew Foote <footeandrew1@gmail.com>
2022-12-09 14:31:49 +0000

    d9p2

--- solutions/python/y2022/d9.py
+++ solutions/python/y2022/d9.py
@@ -11,7 +11,18 @@ L 5
 R 2\
 ''', [
 	('p1', '13'),
-	('p2', '0')
+	('p2', '1')
+]), ('example2', '''\
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20\
+''', [
+	('p2', '36')
 ])]
 
 def parse(ip: str) -> Iterator[tuple[str, int]]:
@@ -73,8 +84,8 @@ def step(moves: Iterable[tuple[str, int]]) -> Iterator[tuple[Point, Point]]:
 def diagram(hpos: Point, tpos: Point) -> str:
 	ymin = min(hpos[0], tpos[0], 0)
 	ymax = max(hpos[0], tpos[0], 0)
-	xmin = min(hpos[1], tpos[1], 1)
-	xmax = max(hpos[1], tpos[1], 1)
+	xmin = min(hpos[1], tpos[1], 0)
+	xmax = max(hpos[1], tpos[1], 0)
 
 	lines = []
 
@@ -110,5 +121,75 @@ def p1(ip: str) -> int:
 
 	return len(visited)
 
+def step2(moves: Iterable[tuple[str, int]]) -> Iterator[list[Point]]:
+	knotpos = [(0, 0) for _ in range(10)]
+	# head is knotpos[0], tail is knotpos[9]
+
+	#print(hpos, tpos)
+	#yield knotpos
+
+	for ds, count in moves:
+		#print('move:', ds, count)
+
+		for _  in range(count):
+			#print('nexstep')
+			d1 = DIRMAP[ds]
+	
+			for i, pos in enumerate(knotpos):
+				#print('knot number', i, 'currently at', pos)
+				knotpos[i] = (pos[0] + d1[0], pos[1] + d1[1])
+				#print('now at', knotpos[i])
+
+				if i < len(knotpos) - 1:
+					#print('should next knot move?')
+					ydif = knotpos[i][0] - knotpos[i + 1][0]
+					xdif = knotpos[i][1] - knotpos[i + 1][1]
+					yunit = ydif // abs(ydif) if ydif else 0
+					xunit = xdif // abs(xdif) if xdif else 0
+					#print('ydif', ydif, 'xdif', xdif, 'yunit', yunit, 'xunit', xunit)
+
+					if abs(ydif) > 1 or abs(xdif) > 1:
+						d1 = (yunit, xunit)
+						#print('yes should move by', d1)
+					else:
+						d1 = (0, 0)
+
+			#print(diagram2(knotpos))
+			#input()
+			yield knotpos
+
+def diagram2(knotpos: list[Point]) -> str:
+	ymin = min(*(pos[0] for pos in knotpos), 0)
+	ymax = max(*(pos[0] for pos in knotpos), 0)
+	xmin = min(*(pos[1] for pos in knotpos), 0)
+	xmax = max(*(pos[1] for pos in knotpos), 0)
+
+	lines = []
+
+	for i in range(ymin, ymax + 1):
+		chars = []
+
+		for j in range(xmin, xmax + 1):
+			for k, pos in enumerate(knotpos):
+				print(pos)
+				if (i, j) == pos:
+					sk = str(k)
+					if sk == '0': sk = 'H'
+					if sk == '9': sk = 'T'
+					chars.append(sk)
+					break
+			else:
+				chars.append('.')
+
+		lines.append(''.join(chars))
+
+	return '\n'.join(lines)
+
 def p2(ip: str) -> int:
-	return 0
+	# num positions tail visited at least once
+	visited = set()
+
+	for knotpos in step2(parse(ip)):
+		visited.add(knotpos[9])
+
+	return len(visited)
