59c60b1139a8ecb2a5fed2de40a34e0f708d69e8
Andrew Foote <footeandrew1@gmail.com>
2022-12-11 10:55:22 +0000

    start timer

---
+++ solutions/python/y2022/d11.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+import itertools as it
+from typing import Iterable, Iterator, Literal
+
+test_inputs = [('example', '''\
+\
+''', [
+	('p1', '0'),
+	('p2', '0')
+])]
+
+
+
+def parse(ip: str) -> Iterator[Monkey]:
+
+def p1(ip: str) -> int:
+	return 0
+
+def p2(ip: str) -> int:
+	return 0
