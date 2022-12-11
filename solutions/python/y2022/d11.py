afd6a6634869ebb0b0d0808f97c4c981981daf67
Andrew Foote <footeandrew1@gmail.com>
2022-12-11 12:15:07 +0000

    d11p2

--- solutions/python/y2022/d11.py
+++ solutions/python/y2022/d11.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from collections import deque
 from dataclasses import dataclass
 import itertools as it
@@ -33,7 +34,7 @@ Monkey 3:
     If false: throw to monkey 1\
 ''', [
 	('p1', '10605'),
-	('p2', '0')
+	('p2', '2713310158')
 ])]
 
 @dataclass
@@ -70,7 +71,6 @@ def parse(ip: str) -> Iterator[Monkey]:
 		yield Monkey(items, op, test, true_branch, false_branch)
 
 def play_round(monkeys: list[Monkey]):
-	print(monkeys)
 	for monkey in monkeys:
 		while monkey.items:
 			item = monkey.items.popleft()
@@ -91,5 +91,143 @@ def p1(ip: str) -> int:
 	monkeys.sort(key=lambda monkey: monkey.inspect_count)
 	return monkeys[-1].inspect_count * monkeys[-2].inspect_count
 
+# @dataclass
+# class Worry(ABC):
+# 	@abstractmethod
+# 	def modulo(self, d: int) -> int:
+# 		...
+
+# @dataclass
+# class SimpWorry(ABC):
+# 	value: int
+
+# 	def modulo(self, d):
+# 		return self.value % d
+
+# @dataclass
+# class AddWorry(ABC):
+# 	base: Worry
+# 	addend: int
+
+# 	def modulo(self, d):
+# 		# a + b equiv c (mod d)
+# 		# iff
+# 		# a equiv c - b (mod d)
+# 		return (self.base.modulo(d) + self.addend) % d
+
+# @dataclass
+# class MulWorry(ABC):
+# 	base: Worry
+# 	muland: int
+
+# 	def modulo(self, d):
+# 		return (self.base.modulo(d) * self.muland) % d
+
+# @dataclass
+# class SquareWorry(ABC):
+# 	base: Worry
+
+# 	def modulo(self, d):
+# 		# given that a equiv c (mod d),
+# 		# solve for x in a^2 equiv x (mod d)
+# 		# a equiv c (mod d) means a = qd + c
+# 		# so a^2 = (qd + c)^2 = q^2 d^2 + 2qdc + c^2
+# 		#                     = (q^2 d + 2qc) d + c^2
+# 		# so x equiv c^2 (mod d)
+# 		return (self.base.modulo(d) ** 2) % d
+
+@dataclass
+class Worry:
+	rs: dict[int, int]
+
+	def __mod__(self, other: int) -> int:
+		return self.rs[other]
+
+	def __add__(self, other: int) -> 'Worry':
+		return Worry({
+			d: (r + other) % d
+			for d, r in self.rs.items() 
+		})
+
+	def __mul__(self, other: int) -> 'Worry':
+		return Worry({
+			d: (r * other) % d
+			for d, r in self.rs.items() 
+		})		
+
+	def __pow__(self, other: int) -> 'Worry':
+		return Worry({
+			d: (r ** other) % d
+			for d, r in self.rs.items()
+		})
+
+@dataclass
+class Monkey2:
+	items: deque[Worry]
+	op: str
+	test: int
+	true_branch: int
+	false_branch: int
+	inspect_count: int=0
+
+	def apply_op(self, worry: Worry) -> Worry:
+		lopnd, optr, ropnd = self.op.split()
+
+		if lopnd == 'old' and ropnd == 'old' and optr == '*':
+			lopnd = worry
+			optr = '^'
+			ropnd = 2
+		else:
+			lopnd = worry if lopnd == 'old' else int(lopnd)
+			ropnd = worry if ropnd == 'old' else int(ropnd)
+
+		return {
+			'+': lambda x, y: x + y,
+			'*': lambda x, y: x * y,
+			'^': lambda x, y: x ** y
+		}[optr](lopnd, ropnd)
+
+	def apply_test(self, worry: Worry) -> bool:
+		return worry % self.test == 0
+
+def parse2(ip: str) -> list[Monkey2]:
+	lines = ip.splitlines()
+	monkeys = []
+	starting_items = []
+
+	for i in range(0, len(lines), 7):
+		items = lines[i + 1].removeprefix('  Starting items: ')
+		starting_items.append([int(item.strip()) for item in items.split(',')])
+		op = lines[i + 2].removeprefix('  Operation: new = ')
+		test = int(lines[i + 3].removeprefix('  Test: divisible by '))
+		true_branch = int(lines[i + 4].removeprefix('    If true: throw to monkey '))
+		false_branch = int(lines[i + 5].removeprefix('    If false: throw to monkey '))
+		monkeys.append(Monkey2(deque([]), op, test, true_branch, false_branch))
+
+	tests = [monkey.test for monkey in monkeys]
+
+	for monkey, sis in zip(monkeys, starting_items):
+		monkey.items.extend(Worry({t: si % t for t in tests}) for si in sis)
+
+	return monkeys
+
+def play_round2(monkeys: list[Monkey2]):
+	for monkey in monkeys:
+		while monkey.items:
+			item = monkey.items.popleft()
+			monkey.inspect_count += 1
+			worry = monkey.apply_op(item)
+
+			if monkey.apply_test(worry):
+				monkeys[monkey.true_branch].items.append(worry)
+			else:
+				monkeys[monkey.false_branch].items.append(worry)
+
 def p2(ip: str) -> int:
-	return 0
+	monkeys = parse2(ip)
+
+	for _ in range(10_000):
+		play_round2(monkeys)
+
+	monkeys.sort(key=lambda monkey: monkey.inspect_count)
+	return monkeys[-1].inspect_count * monkeys[-2].inspect_count
