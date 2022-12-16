b8359a772b82f25d8f1f396422218a8ce8666d29
Andrew Foote <footeandrew1@gmail.com>
2022-12-16 20:15:35 +0000

    d16p1: cleanup

--- solutions/python/lib/graph.py
+++ solutions/python/lib/graph.py
@@ -1,5 +1,26 @@
 import heapq
 
+def dfs(root, children):
+    # https://11011110.github.io/blog/2013/12/17/stack-based-graph-traversal.html
+    # 
+    # I always have to look up this blog post since I can never remember where exactly you do the
+    # check against the visited set.
+
+    visited = {root}
+    stack = [iter(children(root))]
+
+    while stack:
+        try:
+            node = next(stack[-1])
+        except StopIteration:
+            del stack[-1]
+        else:
+            yield node
+
+            if node not in visited:
+                visited.add(node)
+                stack.append(iter(children(node)))
+
 def dijkstra(root, children, cost):
     queue = [(0, root)]
     heapq.heapify(queue)
--- solutions/python/y2022/d16.py
+++ solutions/python/y2022/d16.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
-from enum import Enum
+import functools as ft
 import re
 from solutions.python.lib import graph
-from typing import Iterator, Self
 
 test_inputs = [('example', '''\
 Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -28,83 +27,6 @@ class Valve:
 	flow_rate: int
 	tunnels: list[str]
 
-class ActionKind(Enum):
-	OPEN = 0
-	MOVE = 1
-	WAIT = 2
-
-@dataclass
-class Action:
-	kind: ActionKind
-	valve_i: int
-
-# To apply Dijkstra's algorithm we need to calculate the cost of moving from one state to another,
-# and we need the cost of moving along a path of states to be the sum of those costs. Well, each
-# state-move costs us 1 minute of time, multiplied by the opportunity cost of whatever else we
-# could be doing, I guess. 
-
-# sum over the final 30-state path = total pressure released over the 30 minutes
-# so each edge benefit could be the pressure released during that minute
-# i.e. the pressure released by the open valves for that minute
-
-# Dijkstra's algorithm would allow us to find the path through the state-tree that would maximise
-# benefit of reaching a state with ticks=30; 
-
-# We have a tree of states, of depth exactly 30. Each state has a tick number (from 0 to 30), a
-# current valve index and a set of open valves.
-# 
-# For each state, we can calculate the 
-
-@dataclass(frozen=True)
-class State:
-	valves: list[Valve]
-	ticks: int
-	cur_valve_i: int
-	open_valves: set[int]
-
-	def __hash__(self: Self):
-		return hash((self.ticks, self.cur_valve_i, tuple(sorted(self.open_valves))))
-
-	def __lt__(self: Self, other: Self) -> bool:
-		return self == other
-
-	@property
-	def cur_valve(self: Self) -> Valve:
-		return self.valves[self.cur_valve_i]
-
-	def ppm(self: Self) -> int:
-		return sum(self.valves[valve_i].flow_rate for valve_i in self.open_valves)
-
-	def score(self: Self) -> int:
-		# how much pressure will be released over the remaining minutes
-		return self.ppm() * (TIME_TILL_ERUPTION - self.ticks)
-
-	def actions(self: Self) -> Iterator[Action]:
-		if self.ticks < TIME_TILL_ERUPTION:
-			if self.cur_valve_i not in self.open_valves:
-				yield Action(ActionKind.OPEN, self.cur_valve_i)
-
-			for valve_i in self.cur_valve.tunnels:
-				yield Action(ActionKind.MOVE, valve_i)
-
-			yield Action(ActionKind.WAIT, 0)
-
-	def apply(self: Self, action: Action) -> Self:
-		if action.kind == ActionKind.OPEN:
-			return State(self.valves, self.ticks + 1, self.cur_valve_i, self.open_valves | {action.valve_i})
-
-		if action.kind == ActionKind.MOVE:
-			return State(self.valves, self.ticks + 1, action.valve_i, self.open_valves)
-
-		if action.kind == ActionKind.WAIT:
-			return State(self.valves, self.ticks + 1, self.cur_valve_i, self.open_valves)
-
-		assert False
-
-	def next_states(self: Self) -> Iterator[Self]:
-		for action in self.actions():
-			yield self.apply(action)
-
 def parse(ip: str) -> dict[str, Valve]:
 	r = {}
 
@@ -121,130 +43,41 @@ def parse(ip: str) -> dict[str, Valve]:
 	return r
 
 def p1(ip: str) -> int:
-	# 
-
-	# We need to maximise pressure released
-	# At any point in time we are standing at a certain valve
-	# Opening the current value takes one minute and releases
-	# ressure equal to the valves flow rate per minute after release
-	# so the gain from opening the current valve at X minutes is
-	#
-	#    flow_rate * ( 30 - (X + 1) )
-	#
-	# Alternatively we could move to the valve with
-	#
-	# But we could also open up more valves after the current one
-	# is opened, so the full formula would be
-	#
-	#    (flow_rate + GainPerMinute(ChosenNextSteps)) * ( 30 - (X + 1) )
-
-	# Alternatively we can move to a linked valve via a tunnel,
-	# which also takes a minute
-
-	# So, we have a finite number of "action paths" and we need to
-	# find the one that maximises the presusre released
-
-	# Each action is either "open valve X", "move to valve X",
-	# or "nothing" --- though which valves are available to move to / open
-	# will depend on the current state, and it will never be useful to do
-	# nothng until no actions are left available
-
-	# We are currently standing at valve AA (0)
-
 	valves = parse(ip)
-	state = State(valves, 0, 0, set())
+	valve_children = lambda valve_name: valves[valve_name].tunnels
 
-	# if we start "backwards"
-	# suppose we were at the 29th minute, and still in our initial state.
-	# then there is absolutely nothing we could do to release additional pressure, because any action would take a minute to complete.
-	# so the only thing to do would be to wait
+	# There's no point moving to a valve if all of its descendants (including itself) are already
+	# open. We cache the set of descendants for each valve up-front in order to allow us to check
+	# this quickly during the search.
+	#
+	# (This optimization doesn't really make much of a difference, but I thought of it before I
+	# thought of checking for whether the flow rate of the valve to be opened is zero, and I guess
+	# it's not doing any harm.)
 
-	# ok, what if we were at the 28th minute? well, now we have two options:
-	# - open the current valve... but it has flow rate 0, so that would be useless, instead....
-	# - move to a different valve? but that would take a minute, and then it'd take a minute to open it, so it'd still be useless
-	# in this situation, the only thing we can do is wait, again
-	# however, what if we were at a valve with a positive flow rate?
-	# well, then we would have the choice between three options:
-	# - open the valve (returns: flow rate * 1 minute)
-	# - move to a different valve (returns: 0)
-	# - wait (returns: 0)
-	# so in this case, opening the valve would be best
+	valve_descendants = {
+		valve_name: frozenset(graph.dfs(valve_name, valve_children))
+		for valve_name in valves.keys()
+	}
 
-	# if we assume we can calculate, for each state, the maximal additional pressure we could yield after reaching that state...
-	# 
-
-	# state is a tuple of (ticks, cur_valve_i, open_valves_set)
-
-	import functools as ft
-	depth = 0
-
+	# We have to cache this function to make it work, otherwise it spends too much time repeating
+	# calculations.
 	@ft.cache
-	def maximal_additional_pressure_yield_dumb(state):
-		nonlocal depth
-		ticks, cur_valve_i, open_valves = state
-		# print(' ' * depth + f'{ticks=}, {cur_valve_i=}, {open_valves=}', end='')
-		# input()
-		# print()
+	def maximal_additional_pressure_yield(
+		ticks: int, cur_valve_i: str, open_valves: frozenset[str]
+	) -> int:
 
-		# waiting will always give us 0 additional pressure beyond what is already "baked in"
+		"""Returns the maximum additional pressure we can release (besides what we are already
+		releasing each tick) during the remaining ticks."""
+
+		# If we do nothing, we will release 0 additional pressure.
 		m = 0
 
+		# If we don't have any time left, there's nothing we can do.
 		if ticks < TICKS_TILL_ERUPTION:
-			# we can only carry out actions if there are ticks left to do
 
-			if cur_valve_i not in open_valves:
-				depth += 1
-				open_returns = (
-					valves[cur_valve_i].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1))
-					+ maximal_additional_pressure_yield(
-						(ticks + 1, cur_valve_i, open_valves | {cur_valve_i})
-					)
-				)
-				depth -= 1
+			# If the current valve isn't already open, we can open i. This is pointless if the flow
+			# rate is 0, however.
 
-				if open_returns > m:
-					# print(' ' * depth + f'would be better to open the current valve (valve {cur_valve_i}), giving {open_returns} returns')
-					m = open_returns
-
-			for valve_i in valves[cur_valve_i].tunnels:
-				depth += 1
-				move_returns = maximal_additional_pressure_yield(
-					(ticks + 1, valve_i, open_valves)
-				)
-				depth -= 1
-
-				if move_returns > m:
-					# print(' ' * depth + f'would be better to move to valve {valve_i}, giving {move_returns} returns')
-					m = move_returns
-
-		# print(' ' * depth + f'best returns: {m}')
-		return m
-
-	# return maximal_additional_pressure_yield_dumb((0, 'AA', frozenset()))
-
-	def descendant_valves(valve_i: str) -> frozenset[str]:
-		visited = {valve_i}
-		stack = [iter(valves[valve_i].tunnels)]
-
-		while stack:
-			try:
-				cur = next(stack[-1])
-			except StopIteration:
-				del stack[-1]
-			else:
-				if cur not in visited:
-					visited.add(cur)
-					stack.append(iter(valves[cur].tunnels))
-
-		return frozenset(visited)
-
-	cached_descendant_valves = {i: descendant_valves(i) for i in valves.keys()}
-
-	@ft.cache
-	def maximal_additional_pressure_yield(ticks: int, cur_valve_i: str, open_valves: frozenset[str]) -> int:
-		m = 0
-
-		if ticks < TICKS_TILL_ERUPTION:
 			if cur_valve_i not in open_valves and valves[cur_valve_i].flow_rate:
 				open_returns = (
 					valves[cur_valve_i].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1))
@@ -254,8 +87,11 @@ def p1(ip: str) -> int:
 				if open_returns > m:
 					m = open_returns
 
+			# We could also try moving to another valve. As noted above, this is pointless if the
+			# valve's descendants (including itself) are already open.
+
 			for valve_i in valves[cur_valve_i].tunnels:
-				if valve_i in open_valves and cached_descendant_valves[valve_i].issubset(open_valves):
+				if valve_descendants[valve_i].issubset(open_valves):
 					continue
 
 				move_returns = maximal_additional_pressure_yield(ticks + 1, valve_i, open_valves)
@@ -265,63 +101,7 @@ def p1(ip: str) -> int:
 
 		return m
 
-	# @ft.cache
-	# def maximal_additional_pressure_yield(ticks: int, cur_valve_i: str, open_valves: frozenset[str], dead_valves: frozenset[str]) -> int:
-	# 	m = 0
-
-	# 	if ticks < TICKS_TILL_ERUPTION:
-	# 		if cur_valve_i not in open_valves:
-	# 			new_open_valves = open_valves | {cur_valve_i}
-				
-	# 			if descendant_valves(cur_valve_i).issubset(new_open_valves):
-	# 				new_dead_valves = dead_valves | {cur_valve_i}
-	# 			else:
-	# 				new_dead_valves = dead_valves
-
-	# 			open_returns = (
-	# 				valves[cur_valve_i].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1))
-	# 				+ maximal_additional_pressure_yield(ticks + 1, cur_valve_i, new_open_valves, new_dead_valves)
-	# 			)
-
-	# 			if open_returns > m:
-	# 				m = open_returns
-
-	# 		for valve_i in valves[cur_valve_i].tunnels:
-	# 			if valve_i in dead_valves:
-	# 				continue
-
-	# 			move_returns = maximal_additional_pressure_yield(ticks + 1, valve_i, open_valves, dead_valves)
-
-	# 			if move_returns > m:
-	# 				m = move_returns
-
-	# 	return m
-
-		# it's pointless to go to any valve which is:
-		# (a) already open
-		# (b) can only lead to already-open valves
-		#     [in fact, it's pointless even if it can lead to a closed valve, but we couldn't get there given the remaining ticks]
-		#     [but might not need to go that far with the pruning]
-
-		# so let's go through the open valves and check if they have any descendants that are not-open
-		# hm, that's going to be slow
-		# instead, easiest thing to do would probably be to precalculate the descendant-sets beforehand?
-		# then we can just check, for each open valve, whether its descendant-set is a subset of the open-valve-set
-
-
 	return maximal_additional_pressure_yield(0, 'AA', frozenset())
-
-	# max_benefit = -1
-
-	# for state, cost in graph.dijkstra(state, lambda state: state.next_states(), lambda state, parent: -state.ppm()):
-	# 	benefit = -cost
-
-	# 	if benefit > max_benefit:
-	# 		max_benefit = benefit
-
-	# 	print(f'{state=}, {benefit=}, {max_benefit=}')
-
-	# return max_benefit
 
 def p2(ip: str) -> int:
 	return 0
