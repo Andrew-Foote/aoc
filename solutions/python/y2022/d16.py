2d5145cf50352e7cef3699ae59ce1b5e79f83fe3
Andrew Foote <footeandrew1@gmail.com>
2022-12-16 21:29:46 +0000

    d16p2: we get the correct answer for the example... but too slow for real input

--- solutions/python/y2022/d16.py
+++ solutions/python/y2022/d16.py
@@ -16,7 +16,7 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II\
 ''', [
 	('p1', '1651'),
-	('p2', '0')
+	('p2', '1707')
 ])]
 
 TICKS_TILL_ERUPTION = 30
@@ -59,49 +59,133 @@ def p1(ip: str) -> int:
 		for valve_name in valves.keys()
 	}
 
-	# We have to cache this function to make it work, otherwise it spends too much time repeating
-	# calculations.
+	# # We have to cache this function to make it work, otherwise it spends too much time repeating
+	# # calculations.
+	# @ft.cache
+	# def maximal_additional_pressure_yield(
+	# 	ticks: int, cur_valve: str, open_valves: frozenset[str]
+	# ) -> int:
+
+	# 	"""Returns the maximum additional pressure we can release (besides what we are already
+	# 	releasing each tick) during the remaining ticks."""
+
+	# 	# We can release at least 0 additional pressure by doing nothing.
+	# 	options = [0]
+
+	# 	# Opening a valve takes a minute before it will release any additional pressure, and is
+	# 	# therefore pointless if the eruption is only 1 minute away.
+	# 	if ticks < TICKS_TILL_ERUPTION - 1:
+
+	# 		# Opening a valve with a flow rate of 0 will not release any additional pressure, and
+	# 		# is therefore pointless.
+	# 		if cur_valve not in open_valves and valves[cur_valve].flow_rate:
+	# 			options.append(
+	# 				valves[cur_valve].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1))
+	# 				+ maximal_additional_pressure_yield(
+	# 					ticks + 1, cur_valve, open_valves | {cur_valve}
+	# 				)
+	# 			)
+
+	# 	# Moving to another value will take at least 2 minutes before it allows us to release any
+	# 	# additional pressure, and is therefore pointless if the eruption is only 2 minutes away.
+	# 	if ticks < TICKS_TILL_ERUPTION - 2:
+	# 		for valve in valves[cur_valve].tunnels:
+
+	# 			# Moving to a valve which is already open and whose descendants are all already
+	# 			# open will not allow us to open any more valves and is therefore pointless.
+	# 			if valve_descendants[valve].issubset(open_valves):
+	# 				continue
+
+	# 			options.append(maximal_additional_pressure_yield(ticks + 1, valve, open_valves))
+
+	# 	return max(options)
+
+
 	@ft.cache
 	def maximal_additional_pressure_yield(
-		ticks: int, cur_valve_i: str, open_valves: frozenset[str]
+		ticks: int, cur_valve: str, open_valves: frozenset[str]
 	) -> int:
+		def options_for(cur_valve: str):
+			options = []
 
-		"""Returns the maximum additional pressure we can release (besides what we are already
-		releasing each tick) during the remaining ticks."""
+			if ticks < TICKS_TILL_ERUPTION - 1:
+				if cur_valve not in open_valves and valves[cur_valve].flow_rate:
+					options.append((
+						valves[cur_valve].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1)),
+						cur_valve,
+						frozenset({cur_valve})
+					))
 
-		# If we do nothing, we will release 0 additional pressure.
-		m = 0
+			if ticks < TICKS_TILL_ERUPTION - 2:
+				for valve in valves[cur_valve].tunnels:
+					if valve_descendants[valve].issubset(open_valves):
+						continue
 
-		# If we don't have any time left, there's nothing we can do.
-		if ticks < TICKS_TILL_ERUPTION:
+					options.append((0, valve, frozenset()))
 
-			# If the current valve isn't already open, we can open i. This is pointless if the flow
-			# rate is 0, however.
+			return options
 
-			if cur_valve_i not in open_valves and valves[cur_valve_i].flow_rate:
-				open_returns = (
-					valves[cur_valve_i].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1))
-					+ maximal_additional_pressure_yield(ticks + 1, cur_valve_i, open_valves | {cur_valve_i})
-				)
+		options = [
+			self_immreturn + maximal_additional_pressure_yield(
+				ticks + 1, new_self_valve,
+				open_valves | new_self_open_valves
+			)
+			for self_immreturn, new_self_valve, new_self_open_valves in options_for(cur_valve)
+		]
 
-				if open_returns > m:
-					m = open_returns
+		if options:
+			return max(options)
 
-			# We could also try moving to another valve. As noted above, this is pointless if the
-			# valve's descendants (including itself) are already open.
-
-			for valve_i in valves[cur_valve_i].tunnels:
-				if valve_descendants[valve_i].issubset(open_valves):
-					continue
-
-				move_returns = maximal_additional_pressure_yield(ticks + 1, valve_i, open_valves)
-
-				if move_returns > m:
-					m = move_returns
-
-		return m
+		return 0
 
 	return maximal_additional_pressure_yield(0, 'AA', frozenset())
 
 def p2(ip: str) -> int:
-	return 0
+	valves = parse(ip)
+	valve_children = lambda valve_name: valves[valve_name].tunnels
+
+	valve_descendants = {
+		valve_name: frozenset(graph.dfs(valve_name, valve_children))
+		for valve_name in valves.keys()
+	}
+
+	@ft.cache
+	def maximal_additional_pressure_yield(
+		ticks: int, self_valve: str, ele_valve: str, open_valves: frozenset[str]
+	) -> int:
+		def options_for(cur_valve: str):
+			options = []
+
+			if ticks < TICKS_TILL_ERUPTION - 1:
+				if cur_valve not in open_valves and valves[cur_valve].flow_rate:
+					options.append((
+						valves[cur_valve].flow_rate * (TICKS_TILL_ERUPTION - (ticks + 1)),
+						cur_valve,
+						frozenset({cur_valve})
+					))
+
+			if ticks < TICKS_TILL_ERUPTION - 2:
+				for valve in valves[cur_valve].tunnels:
+					if valve_descendants[valve].issubset(open_valves):
+						continue
+
+					options.append((0, valve, frozenset()))
+
+			return options
+
+		options = [
+			self_immreturn + ele_immreturn + maximal_additional_pressure_yield(
+				ticks + 1, new_self_valve, new_ele_valve,
+				open_valves | new_self_open_valves | new_ele_open_valves
+			)
+			for self_immreturn, new_self_valve, new_self_open_valves in options_for(self_valve)
+			for ele_immreturn, new_ele_valve, new_ele_open_valves in options_for(ele_valve)
+			if new_self_open_valves.isdisjoint(new_ele_open_valves)
+		]
+
+		if options:
+			return max(options)
+
+		return 0
+
+	return maximal_additional_pressure_yield(4, 'AA', 'AA', frozenset())
