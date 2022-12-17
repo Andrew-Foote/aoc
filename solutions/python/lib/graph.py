e009b640153707e32e6de7b25b5e5e3d39e0650d
Andrew Foote <footeandrew1@gmail.com>
2022-12-17 04:51:21 +0000

    graph lib

--- solutions/python/lib/graph.py
+++ solutions/python/lib/graph.py
@@ -1,14 +1,15 @@
 from collections import deque
+from collections.abc import Iterator
 import heapq
-from typing import Iterator, Protocol, Self
+from typing import Protocol, Self, TypeVar
 
 _T = TypeVar('_T')
 
-class ChildYielder(Protocol[_T]):
+class ChildGenerator(Protocol[_T]):
     def __call__(self: Self, node: _T) -> Iterator[_T]:
         """Yield the children of the given node in order."""
 
-def dfs(root: _T, children: ChildYielder[_T]) -> Iterator[_T]:
+def dfs(root: _T, children: ChildGenerator[_T]) -> Iterator[_T]:
     # https://11011110.github.io/blog/2013/12/17/stack-based-graph-traversal.html
     # 
     # I always have to look up this blog post since I can never remember where exactly you do the
@@ -28,7 +29,7 @@ def dfs(root: _T, children: ChildYielder[_T]) -> Iterator[_T]:
                 visited.add(node)
                 stack.append(iter(children(node)))
 
-def bfs(root: _T, children: ChildYielder[_T]):
+def bfs(root: _T, children: ChildGenerator[_T]):
     queue: deque[Iterator[_T]] = deque([iter([root])])
     visited: set[_T] = set()
 
@@ -41,18 +42,48 @@ def bfs(root: _T, children: ChildYielder[_T]):
             if node not in visited:
                 yield node
                 visited.add(node)
-                stack.append(iter(children(node)))
+                queue.appendleft(iter(children(node)))
 
-def dijkstra(root: _T, children: ChildYielder[_T]):
+def dijkstra(root: _T, children: ChildGenerator[_T]):
     queue: list[_T] = [root]
     heapq.heapify(queue)
-    visited: set[_T] = set()
+    visited: set[_T] = {root}
 
     while queue:
-        node = heapq.heappop(queue)
+        node: _T = heapq.heappop(queue)
         yield node
 
         for child in children(node):
             if child not in visited:
                 visited.add(child)
                 heapq.heappush(queue, child)
+
+def paradfs(root: _T, children: ChildGenerator[_T]):
+    stack: list[_T] = [root]
+    visited: set[_T] = {root}
+
+    while stack:
+        node: _T = stack.pop()
+        yield node
+
+        for child in children(node):
+            if child not in visited:
+                visited.add(child)
+                stack.append(child)
+
+if __name__ == '__main__':
+    example = {
+        's': 'ac',
+        'a': 'sbd',
+        'b': 'ae',
+        'c': 'sdf',
+        'd': 'aceg',
+        'e': 'bdh',
+        'f': 'cg',
+        'g': 'dfh',
+        'h': 'eg'
+    }
+
+    children = lambda node: example[node]
+    assert list(dfs('s', children)) == ['s', 'a', 'b', 'e', 'd', 'c', 'f', 'g', 'h']
+    assert list(bfs('s', children)) == ['s', 'a', 'c', 'b', 'd', 'f', 'e', 'g', 'h']
---
+++ solutions/python/y2022/d17.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+import functools as ft
+import itertools as it
+import re
+from solutions.python.lib import graph
+
+test_inputs = [('example', '''\
+\
+''', [
+	('p1', '0'),
+	('p2', '0')
+])]
+
+def parse(ip: str) -> None:
+	return None
+
+def p1(ip: str) -> int:
+	return 0
+
+def p2(ip: str) -> int:
+	return 0
