e5d1bfb494e05ab54658a18d384a5a311faf87cf
Andrew Foote <footeandrew1@gmail.com>
2022-12-18 18:41:39 +0000

    formatting

--- solutions/python/lib/digits.py
+++ solutions/python/lib/digits.py
@@ -4,117 +4,117 @@ from typing import overload, TypeVar
 
 @ft.lru_cache(10)
 def log_of_2(b):
-	return math.log(2, b)
+    return math.log(2, b)
 
 LOG10_2 = math.log10(2)
 
 def digit_count(n: int, b: int=10) -> int:
-	"""Count the digits in n's base-b expansion.
+    """Count the digits in n's base-b expansion.
 
-	Based on this StackOverflow answer: https://stackoverflow.com/a/74099536/19822079
+    Based on this StackOverflow answer: https://stackoverflow.com/a/74099536/19822079
 
-	We start with the bit count, which is equal to [log_2 n] + 1 (square brackets denote flooring),
-	so subtracting 1 gives us [log_2 n], i.e. the unique integer k such that
+    We start with the bit count, which is equal to [log_2 n] + 1 (square brackets denote flooring),
+    so subtracting 1 gives us [log_2 n], i.e. the unique integer k such that
 
       log_2 n - 1 <= k < log_2 n.                                              (1)
 
-	From the change-of-base formula for logarithms, we have
+    From the change-of-base formula for logarithms, we have
 
-	  log_2 n = log_b n / log_b 2
+      log_2 n = log_b n / log_b 2
 
-	and hence
+    and hence
 
-	  log_b n = log_2 n * log_b 2.
+      log_b n = log_2 n * log_b 2.
 
-	Multiplying (1) through by log_b 2 therefore gives us
+    Multiplying (1) through by log_b 2 therefore gives us
 
-	  log_b n - log_b 2 < k log_b 2 <= log_b n.
+      log_b n - log_b 2 < k log_b 2 <= log_b n.
 
-	Given that b > 1, we have log_b 2 <= 1, hence log_b n - 1 <= log_b n - log_b 2, so
+    Given that b > 1, we have log_b 2 <= 1, hence log_b n - 1 <= log_b n - log_b 2, so
 
-	  log_b n - 1 < k log_b 2 <= log_b n,
+      log_b n - 1 < k log_b 2 <= log_b n,
 
-	hence
+    hence
 
-	  [log_b n] - 1 = [log_b n - 1] <= [k log_b 2] <= [log_b n].
+      [log_b n] - 1 = [log_b n - 1] <= [k log_b 2] <= [log_b n].
 
-	So [k log_b 2] could be either [log_b n] or [log_b n] - 1. To check for the latter possibility
-	we can calculate b^[k log_b 2]. Since
+    So [k log_b 2] could be either [log_b n] or [log_b n] - 1. To check for the latter possibility
+    we can calculate b^[k log_b 2]. Since
 
-	  log_b n - 1 < [log_b n] <= log_b n and log_b n - 2 < [log_b n] - 1 <= log_b n - 1,
+      log_b n - 1 < [log_b n] <= log_b n and log_b n - 2 < [log_b n] - 1 <= log_b n - 1,
 
-	we have
+    we have
 
-	  n / b < b^[log_b n] <= n and n / b^2 < b^([log_b n] - 1) <= n / b,
+      n / b < b^[log_b n] <= n and n / b^2 < b^([log_b n] - 1) <= n / b,
 
-	so we have [k log_b 2] = [log_b n] - 1 iff b^[k log_b 2] <= n / b, i.e. b^[k log_b 2 + 1] <= n.
-	"""
+    so we have [k log_b 2] = [log_b n] - 1 iff b^[k log_b 2] <= n / b, i.e. b^[k log_b 2 + 1] <= n.
+    """
 
-	k = n.bit_length() - 1
-	e = int(k * log_of_2(b)) + 1
-	return e + (b ** e <= n)
+    k = n.bit_length() - 1
+    e = int(k * log_of_2(b)) + 1
+    return e + (b ** e <= n)
 
 _T = TypeVar('_T')
 
 class digits:
-	__slots__ = ('n', 'b')
+    __slots__ = ('n', 'b')
 
-	n: int
-	b: int
+    n: int
+    b: int
 
-	def __init__(self, n: int, b: int=10):
-		self.n = n
-		self.b = b
+    def __init__(self, n: int, b: int=10):
+        self.n = n
+        self.b = b
 
-	def __repr__(self) -> str:
-		return f'digits({self.n}, {self.b})'
+    def __repr__(self) -> str:
+        return f'digits({self.n}, {self.b})'
 
-	def __eq__(self, other) -> bool:
-		return isinstance(other, digits) and self.n == other.n and self.b == other.b
+    def __eq__(self, other) -> bool:
+        return isinstance(other, digits) and self.n == other.n and self.b == other.b
 
-	def __len__(self) -> int:
-		return digit_count(self.n, self.b)
+    def __len__(self) -> int:
+        return digit_count(self.n, self.b)
 
-	def __iter__(self):
-		n = self.n
-		b = self.b
+    def __iter__(self):
+        n = self.n
+        b = self.b
 
-		while n:
-			n, r = divmod(n, b)
-			yield r
+        while n:
+            n, r = divmod(n, b)
+            yield r
 
-	def __reversed__(self):
-		for i in range(len(self) - 1, -1, -1):
-			yield self[i]
+    def __reversed__(self):
+        for i in range(len(self) - 1, -1, -1):
+            yield self[i]
 
-	def __getitem__(self, place: int) -> int:
-		if not 0 <= place < len(self):
-			raise IndexError(f'no digit at place {place}')
+    def __getitem__(self, place: int) -> int:
+        if not 0 <= place < len(self):
+            raise IndexError(f'no digit at place {place}')
 
-		b = self.b
-		return (self.n % b ** (place + 1)) // b ** place
+        b = self.b
+        return (self.n % b ** (place + 1)) // b ** place
 
-	@overload
-	def get(self, place: int, default: int=0) -> int:
-		...
+    @overload
+    def get(self, place: int, default: int=0) -> int:
+        ...
 
-	@overload
-	def get(self, place: int, default: _T) -> int | _T:
-		...
+    @overload
+    def get(self, place: int, default: _T) -> int | _T:
+        ...
 
-	def get(self, place, default):
-		try:
-			return self[place]
-		except IndexError:
-			return default
+    def get(self, place, default):
+        try:
+            return self[place]
+        except IndexError:
+            return default
 
-	def __setitem__(self, place: int, d: int) -> None:
-		b = self.b
+    def __setitem__(self, place: int, d: int) -> None:
+        b = self.b
 
-		if not 0 <= d < b:
-			raise ValueError(f'{d} is not an allowed digit for base {b}')
+        if not 0 <= d < b:
+            raise ValueError(f'{d} is not an allowed digit for base {b}')
 
-		self.n += (d - self[place]) * b ** place
+        self.n += (d - self[place]) * b ** place
 
 def digit(n: int, place: int, b: int=10) -> int:
-	return digits(n, b)[place]
+    return digits(n, b)[place]
--- solutions/python/lib/grid.py
+++ solutions/python/lib/grid.py
@@ -7,79 +7,79 @@ NESW: tuple[gint, gint, gint, gint] = (gint(0, -1), gint(1, 0), gint(0, 1), gint
 
 @dataclass
 class Line:
-	"""A straight, un-directed line in the plane extending infinitely in both directions.
+    """A straight, un-directed line in the plane extending infinitely in both directions.
 
-	The attributes `x_coeff`, `y_coeff` and `const_coeff` give an equation for the line:
+    The attributes `x_coeff`, `y_coeff` and `const_coeff` give an equation for the line:
 
-	  ax + by + c = 0,
+      ax + by + c = 0,
 
-	where a = `x_coeff`, b = `y_coeff`, c = `const_coeff`. The coefficients are required to be 
-	integers.
-	"""
+    where a = `x_coeff`, b = `y_coeff`, c = `const_coeff`. The coefficients are required to be 
+    integers.
+    """
 
-	x_coeff: int
-	y_coeff: int
-	const_coeff: int
+    x_coeff: int
+    y_coeff: int
+    const_coeff: int
 
-	def __post_init__(self: Self) -> None:
-		if not (self.x_coeff or self.y_coeff):
-			raise ValueError('invalid line: both coefficients are zero')
+    def __post_init__(self: Self) -> None:
+        if not (self.x_coeff or self.y_coeff):
+            raise ValueError('invalid line: both coefficients are zero')
 
-	def coeffs(self) -> tuple[int, int, int]:
-		return self.x_coeff, self.y_coeff, self.const_coeff
+    def coeffs(self) -> tuple[int, int, int]:
+        return self.x_coeff, self.y_coeff, self.const_coeff
 
-	def __contains__(self: Self, p: gint) -> bool:
-		return self.x_coeff * p.real + self.y_coeff * p.imag == -self.const_coeff		
+    def __contains__(self: Self, p: gint) -> bool:
+        return self.x_coeff * p.real + self.y_coeff * p.imag == -self.const_coeff       
 
-	def __and__(self: Self, other: Self) -> Self | complex | None:
-		"""Returns the intersection of the two lines.
+    def __and__(self: Self, other: Self) -> Self | complex | None:
+        """Returns the intersection of the two lines.
 
-		If the two lines are the very same line, the result is that line. If the two lines are
-		parallel, the result is `None`. Otherwise, the result is the unique point of intersection
-		of the two lines.
+        If the two lines are the very same line, the result is that line. If the two lines are
+        distinct and parallel, the result is `None`. Otherwise, the result is the unique point of
+        intersection of the two lines.
 
-		If the two lines are given by the equations
+        If the two lines are given by the equations
 
-		  ax + by + c = 0,
-		  Ax + By + C = 0,
+          ax + by + c = 0,
+          Ax + By + C = 0,
 
-		then we can find their intersection by solving those two equations simultaneously. This
-		can be done using Gaussian elimination.
+        then we can find their intersection by solving those two equations simultaneously. This
+        can be done using Gaussian elimination.
 
-		First, we reorder the equations so that the leading coefficient of the first one is either
-		in the same colum or to the left of the leading coefficient of the second one. That is, if
-		a = 0 but A != 0, we swap the two equations. 
+        First, we reorder the equations so that the leading coefficient of the first one is either
+        in the same colum or to the left of the leading coefficient of the second one. That is, if
+        a = 0 but A != 0, we swap the two equations. 
 
-		Next, we need to make sure that the leading coefficient of the first equation is in fact
-		to the left of the leading coefficient of the second equation. There are two cases to
-		consider:
+        Next, we need to make sure that the leading coefficient of the first equation is in fact
+        to the left of the leading coefficient of the second equation. There are two cases to
+        consider:
 
-		- If the leading coefficient of the first equation is b, our system of equations is just
+        - If the leading coefficient of the first equation is b, our system of equations is just
 
-		    by + c = 0,
-		    By + C = 0.
+            by + c = 0,
+            By + C = 0.
 
-		  Adding the first equation scaled by -B/b to the second, and dividing the first equation
-		  through by 1/b, we get
+          Adding the first equation scaled by -B/b to the second, and dividing the first equation
+          through by 1/b, we get
 
-		     y + c/b      = 0,
-		         C - Bc/b = 0.
+             y + c/b      = 0,
+                 C - Bc/b = 0.
 
-		  If C - Bc/b turns out to be zero (i.e. bC = Bc), then the second equation is always
-		  satisfied, so the solutions are all points (x, y) such that y = -c/b, and the two lines
-		  must be the very same line. Otherwise, the second equation cannot be satisfied and hence
-		  the lines do not intersect.
+          If C - Bc/b turns out to be zero (i.e. bC = Bc), then the second equation is always
+          satisfied, so the solutions are all points (x, y) such that y = -c/b, and the two lines
+          must be the very same line. Otherwise, the second equation cannot be satisfied and hence
+          the lines do not intersect.
 
-		- If the leading coefficient of the first equation is a, we add the first equation scaled
-		  by -A/a to the second and divide the first equation through by a, giving
+        - If the leading coefficient of the first equation is a, we add the first equation scaled
+          by -A/a to the second and divide the first equation through by a, giving
 
-		    x + (b/a)     y + c/a      = 0,
-		        (B - Ab/a)y + C - Ac/a = 0.
+            x + (b/a)     y + c/a      = 0,
+                (B - Ab/a)y + C - Ac/a = 0.
 
-		  We can rewrite this as
+          We can rewrite this as
 
-		    x + (b/a)     y = -c/a,
-		        (B - Ab/a)y = Ac/a - C.
+            x + (b/a)     y = -c/a,
+                (B - Ab/a)y = Ac/a - C.
 
           Then there are three possibilities to consider:
 
@@ -107,247 +107,252 @@ class Line:
                 = -(aBc - abC)/(a(aB - Ab))
                 = -(Bc - bC)/(aB - Ab)
                 = (bC - Bc)/(aB - Ab).
-		"""
-		a, b, c = self.coeffs()
-		A, B, C = other.coeffs()
+        """
+        a, b, c = self.coeffs()
+        A, B, C = other.coeffs()
 
-		if a == 0:
-			if A != 0:
-				return other & self
+        if a == 0:
+            if A != 0:
+                return other & self
 
-			if b * C == B * c:
-				return self
+            if b * C == B * c:
+                return self
 
-			return None
+            return None
 
-		if a * B == A * b:
-			if A * c == a * C:
-				return self
+        if a * B == A * b:
+            if A * c == a * C:
+                return self
 
-			return None
+            return None
 
-		d = a * B - A * b
-		return complex((b * C - B * c) / d, (A * c - C * a) / d)
+        d = a * B - A * b
+        return complex((b * C - B * c) / d, (A * c - C * a) / d)
 
-	def __eq__(self: Self, other: object) -> bool:
-		"""Returns True if the two lines are the same as sets of points."""
+    def __eq__(self: Self, other: object) -> bool:
+        """Returns True if the two lines are the same as sets of points."""
 
-		if not isinstance(other, Line):
-			return False
+        if not isinstance(other, Line):
+            return False
 
-		a, b, c = self.coeffs()
-		A, B, C = other.coeffs()
+        a, b, c = self.coeffs()
+        A, B, C = other.coeffs()
 
-		return a * B == A * b and A * c == a * C and (a != 0 or A != 0 or b * C == B * c)
-		#return isinstance(self & other, self.__class__)
+        return a * B == A * b and A * c == a * C and (a != 0 or A != 0 or b * C == B * c)
+        #return isinstance(self & other, self.__class__)
 
-	def gradient(self: Self) -> float:
-		return -self.x_coeff / self.y_coeff
+    def gradient(self: Self) -> float:
+        return -self.x_coeff / self.y_coeff
 
-	def intercept(self: Self) -> float:
-		return -self.const_coeff / self.y_coeff
-		
-	@classmethod
-	def fromeq(cls: Type[Self], m: int, c: int) -> Self:
-		# y = mx + c
-		# <==>
-		# -mx + y - c = 0
-		return cls(-m, 1, -c)
+    def intercept(self: Self) -> float:
+        return -self.const_coeff / self.y_coeff
+        
+    @classmethod
+    def fromeq(cls: Type[Self], m: int, c: int) -> Self:
+        # y = mx + c
+        # <==>
+        # -mx + y - c = 0
+        return cls(-m, 1, -c)
 
-	@classmethod
-	def from2points(cls: Type[Self], p0: gint, p1: gint) -> Self:
-		# y = y0 + (y1 - y0)/(x1 - x0) * (x - x0)
-		# <==>
-		# -(y1 - y0)/(x1 - x0) x + y + x0 (y1 - y0)/(x1 - x0) - y0 = 0
-		# <==>
-		# (y0 - y1)x + (x1 - x0)y + x0 (y1 - y0) - y0 (x1 - x0) = 0
+    @classmethod
+    def from2points(cls: Type[Self], p0: gint, p1: gint) -> Self:
+        # y = y0 + (y1 - y0)/(x1 - x0) * (x - x0)
+        # <==>
+        # -(y1 - y0)/(x1 - x0) x + y + x0 (y1 - y0)/(x1 - x0) - y0 = 0
+        # <==>
+        # (y0 - y1)x + (x1 - x0)y + x0 (y1 - y0) - y0 (x1 - x0) = 0
 
-		x0, y0 = gint_rect(p0)
-		x1, y1 = gint_rect(p1)
-		dx = x1 - x0
-		dy = y1 - y0
-		# we should have a Gaussian integer type, eally
-		return cls(-dy, dx, x0 * dy - y0 * dx)
+        x0, y0 = gint_rect(p0)
+        x1, y1 = gint_rect(p1)
+        dx = x1 - x0
+        dy = y1 - y0
+        # we should have a Gaussian integer type, eally
+        return cls(-dy, dx, x0 * dy - y0 * dx)
 
-		return cls(-dy, dx, p0.real * dy - p0.imag * dx)
+        return cls(-dy, dx, p0.real * dy - p0.imag * dx)
 
 @dataclass
 class Rect:
-	"""A rectangular region in a grid.
+    """A rectangular region in a grid.
 
-	The bounds are inclusive, i.e. a Rect whose top and bottom are the same is considered to
-	include a single row of points."""
-	top: int
-	right: int
-	bottom: int
-	left: int
+    The bounds are inclusive, i.e. a Rect whose top and bottom are the same is considered to
+    include a single row of points."""
+    top: int
+    right: int
+    bottom: int
+    left: int
 
-	def __post_init__(self: Self) -> None:
-		if self.top > self.bottom:
-			raise ValueError(f'bottom ({self.bottom}) exceeds top ({self.top})')
+    def __post_init__(self: Self) -> None:
+        if self.top > self.bottom:
+            raise ValueError(f'bottom ({self.bottom}) exceeds top ({self.top})')
 
-		if self.left > self.right:
-			raise ValueError(f'left ({self.left}) exceeds right ({self.right})')
+        if self.left > self.right:
+            raise ValueError(f'left ({self.left}) exceeds right ({self.right})')
 
-	@property
-	def width(self: Self) -> int:
-		return self.right - self.left + 1
+    @property
+    def width(self: Self) -> int:
+        return self.right - self.left + 1
 
-	@property
-	def height(self: Self) -> int:
-		return self.bottom - self.top + 1
+    @property
+    def height(self: Self) -> int:
+        return self.bottom - self.top + 1
 
-	@property
-	def top_left(self: Self) -> gint:
-		return gint(self.left, self.top)
+    @property
+    def top_left(self: Self) -> gint:
+        return gint(self.left, self.top)
 
-	@property
-	def top_right(self: Self) -> gint:
-		return gint(self.right, self.top)
+    @property
+    def top_right(self: Self) -> gint:
+        return gint(self.right, self.top)
 
-	@property
-	def bottom_right(self: Self) -> gint:
-		return gint(self.right, self.bottom)
+    @property
+    def bottom_right(self: Self) -> gint:
+        return gint(self.right, self.bottom)
 
-	@property
-	def bottom_left(self: Self) -> gint:
-		return gint(self.left, self.bottom)
+    @property
+    def bottom_left(self: Self) -> gint:
+        return gint(self.left, self.bottom)
 
-	@classmethod
-	def bounding(cls: Type[Self], points: Iterable[gint]) -> Self:
-		iterator = iter(points)
-		first = next(iterator)
+    @classmethod
+    def bounding(cls: Type[Self], points: Iterable[gint]) -> Self:
+        iterator = iter(points)
+        first = next(iterator)
 
-		left = right = first.real
-		top = bottom = first.imag
+        left = right = first.real
+        top = bottom = first.imag
 
-		for p in iterator:
-			if p.real < left:
-				left = p.real
-			elif p.real > right:
-				right = p.real
+        for p in iterator:
+            if p.real < left:
+                left = p.real
+            elif p.real > right:
+                right = p.real
 
-			if p.imag < top:
-				top = p.imag
-			elif p.imag > bottom:
-				bottom = p.imag
+            if p.imag < top:
+                top = p.imag
+            elif p.imag > bottom:
+                bottom = p.imag
 
-		return cls(top, right, bottom, left)
+        return cls(top, right, bottom, left)
 
-	def __contains__(self: Self, point: gint) -> bool:
-		return self.left <= point.real <= self.right and self.top <= point.imag <= self.bottom
+    def __contains__(self: Self, point: gint) -> bool:
+        return self.left <= point.real <= self.right and self.top <= point.imag <= self.bottom
 
-	def __and__(self: Self, other: Self) -> Optional[Self]:
-		try:
-			return self.__class__(max(self.top, other.top), min(self.right, other.right), min(self.bottom, other.bottom), max(self.left, other.left))
-		except ValueError:
-			return None
+    def __and__(self: Self, other: Self) -> Optional[Self]:
+        try:
+            return self.__class__(
+                max(self.top, other.top),
+                min(self.right, other.right),
+                min(self.bottom, other.bottom),
+                max(self.left, other.left)
+            )
+        except ValueError:
+            return None
 
-	def __iter__(self: Self) -> Iterator[gint]:
-		for y in range(self.height):
-			for x in range(self.width):
-				yield self.top_left + gint(x, y)
+    def __iter__(self: Self) -> Iterator[gint]:
+        for y in range(self.height):
+            for x in range(self.width):
+                yield self.top_left + gint(x, y)
 
 @dataclass
 class Path:
-	"""A path in a grid made up of rows and columns of tiles."""
+    """A path in a grid made up of rows and columns of tiles."""
 
-	points: list[gint]
+    points: list[gint]
 
-	def __init__(self: Self, points: Iterable[gint]) -> None:
-		self.points = list(points)
+    def __init__(self: Self, points: Iterable[gint]) -> None:
+        self.points = list(points)
 
-		if not self.points:
-			raise ValueError('path cannot be empty')
+        if not self.points:
+            raise ValueError('path cannot be empty')
 
-		dupe_indices = []
+        dupe_indices = []
 
-		for i, (p, q) in enumerate(zip(self.points, self.points[1:])):
-			if p == q:
-				dupe_indices.append(i + 1)
+        for i, (p, q) in enumerate(zip(self.points, self.points[1:])):
+            if p == q:
+                dupe_indices.append(i + 1)
 
-			if p.real != q.real and p.imag != q.imag:
-				raise ValueError(f'{i}th path segment ({p} to {q}) is diagonal')
+            if p.real != q.real and p.imag != q.imag:
+                raise ValueError(f'{i}th path segment ({p} to {q}) is diagonal')
 
-		for i in dupe_indices[::-1]:
-			del self.points[i]
+        for i in dupe_indices[::-1]:
+            del self.points[i]
 
-	def __contains__(self: Self, point: gint) -> bool:
-		return any(point in Rect.bounding((p, q)) for p, q in zip(self.points, self.points[1:]))
+    def __contains__(self: Self, point: gint) -> bool:
+        return any(point in Rect.bounding((p, q)) for p, q in zip(self.points, self.points[1:]))
 
-	def __iter__(self: Self) -> Iterator[gint]:
-		yield self.points[0]
+    def __iter__(self: Self) -> Iterator[gint]:
+        yield self.points[0]
 
-		for p, q in zip(self.points, self.points[1:]):
-			d = q - p
-			m = int(abs(d))
-			assert m > 0
-			u = d / m
+        for p, q in zip(self.points, self.points[1:]):
+            d = q - p
+            m = int(abs(d))
+            assert m > 0
+            u = d / m
 
-			for _ in range(m):
-				p += u
-				yield p
+            for _ in range(m):
+                p += u
+                yield p
 
 T = TypeVar('T')
 
 @dataclass
 class Grid(Generic[T]):
-	rows: list[list[T]]
-	origin: gint
+    rows: list[list[T]]
+    origin: gint
 
-	def __init__(self: Self, rows: Iterable[Iterable[T]], origin: gint=gint()) -> None:
-		self.rows = [list(row) for row in rows]
+    def __init__(self: Self, rows: Iterable[Iterable[T]], origin: gint=gint()) -> None:
+        self.rows = [list(row) for row in rows]
 
-		if not self.rows:
-			raise ValueError('grid is empty')
+        if not self.rows:
+            raise ValueError('grid is empty')
 
-		if not all(len(row) == len(self.rows[0]) for row in self.rows[1:]):
-			raise ValueError('rows are not all same length')
+        if not all(len(row) == len(self.rows[0]) for row in self.rows[1:]):
+            raise ValueError('rows are not all same length')
 
-		self.origin = origin
+        self.origin = origin
 
-	@property
-	def width(self) -> int:
-		return len(self.rows[0])
+    @property
+    def width(self) -> int:
+        return len(self.rows[0])
 
-	@property
-	def height(self) -> int:
-		return len(self.rows)
+    @property
+    def height(self) -> int:
+        return len(self.rows)
 
-	def rect(self):
-		return Rect.bounding((self.origin, self.origin + gint(self.width - 1, self.height - 1)))
+    def rect(self):
+        return Rect.bounding((self.origin, self.origin + gint(self.width - 1, self.height - 1)))
 
-	@classmethod
-	def fromrect(cls: Type[Self], rect: Rect, label: Callable[[gint], T]) -> Self:
-		return cls(
-			(
-				(label(gint(x, y)) for x in range(rect.left, rect.right + 1))
-				for y in range(rect.top, rect.bottom + 1)
-			),
-			rect.top_left
-		)
+    @classmethod
+    def fromrect(cls: Type[Self], rect: Rect, label: Callable[[gint], T]) -> Self:
+        return cls(
+            (
+                (label(gint(x, y)) for x in range(rect.left, rect.right + 1))
+                for y in range(rect.top, rect.bottom + 1)
+            ),
+            rect.top_left
+        )
 
-	def __getitem__(self, p: gint) -> T:
-		return self.rows[p.imag - self.origin.imag][p.real - self.origin.real]
+    def __getitem__(self, p: gint) -> T:
+        return self.rows[p.imag - self.origin.imag][p.real - self.origin.real]
 
-	def __setitem__(self, p: gint, v: T) -> None:
-		self.rows[p.imag - self.origin.imag][p.real - self.origin.real] = v
+    def __setitem__(self, p: gint, v: T) -> None:
+        self.rows[p.imag - self.origin.imag][p.real - self.origin.real] = v
 
 @dataclass
 class DefaultGrid(Generic[T]):
-	"""An infinite grid whose values outside of a finite region are computed by a callback."""
+    """An infinite grid whose values outside of a finite region are computed by a callback."""
 
-	default: Callable[[gint], T]
-	entries: dict[gint, T] = field(default_factory=lambda: {})
+    default: Callable[[gint], T]
+    entries: dict[gint, T] = field(default_factory=lambda: {})
 
-	def __getitem__(self, p: gint) -> T:
-		try:
-			return self.entries[p]
-		except KeyError:
-			return self.default(p)
+    def __getitem__(self, p: gint) -> T:
+        try:
+            return self.entries[p]
+        except KeyError:
+            return self.default(p)
 
-	def __setitem__(self, p: gint, v: T) -> None:
-		self.entries[p] = v
+    def __setitem__(self, p: gint, v: T) -> None:
+        self.entries[p] = v
 
-	def rect(self):
-		return Rect.bounding(self.entries.keys())
+    def rect(self):
+        return Rect.bounding(self.entries.keys())
