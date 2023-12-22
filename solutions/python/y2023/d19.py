7c31f1acd063fb88b0d869e80047b509b628c429
Andrew Foote <footeandrew1@gmail.com>
2023-12-22 22:55:22 +0000

    d19 p2

--- solutions/python/y2023/d19.py
+++ solutions/python/y2023/d19.py
@@ -4,6 +4,7 @@ import operator
 import re
 from typing import Callable, Iterator
 from utils import joinlines
+from solutions.python.lib.utils import prod, range_intersection
 
 test_inputs = [
 	('example', '''\
@@ -32,6 +33,7 @@ hdj{m>838:A,pv}
 {x=2127,m=1623,a=2188,s=1013}: in -> px -> rfg -> A'''),
 		('rating_sums_csv', '7540,4623,6951'),
 		('p1', 19114),
+		('p2', 167409079868000)
 	])
 ]
 
@@ -153,3 +155,135 @@ def rating_sums_csv(ip: str) -> str:
 
 def p1(ip: str) -> int:
 	return sum(rating_sums(ip))
+
+def p2_bruteforce(ip: str) -> int:
+	workflows, _ = parse(ip)
+	k = 0
+
+	for x in range(1, 4001):
+		for m in range(1, 4001):
+			for a in range(1, 4001):
+				for s in range(1, 4001):
+					t = trace(workflows, {'x': x, 'm': m, 'a': a, 's': s})
+					print(t)
+					input()
+					k += t[-1] == 'A'
+
+	return k
+
+#                        a<2006 :          qkq
+#       s<1351  : px  -> 
+#                        a>=2006,m>2090 :  [A]
+#                        a>=2006,m<=2090 : rfg
+# in -> 
+#       s>=1351 : qqz
+
+# if s < 1351, a >= 2006, m > 2090, then A
+
+# so for a given workflow, we can compute a map of conditions => next workflow
+# and we can iteratively expand this map
+# a condition will ultimately be a set of ranges
+# which the s, a, m, x must fall into
+
+Cond = dict[str, range]
+
+def null_cond() -> Cond:
+	return {attr: range(1, 4001) for attr in 'xmas'}
+
+def conjoin_conds(cond1: Cond, cond2: Cond) -> Cond:
+	return {attr: range_intersection(cond1[attr], cond2[attr]) for attr in 'xmas'}
+
+def conjoin_cond_with_rule(cond: Cond, rule: Rule) -> Cond:
+	cond_range = cond[rule.attr]
+
+	if rule.op == Operator.LT:
+		rule_range = range(1, rule.val)
+	elif rule.op == Operator.GT:
+		rule_range = range(rule.val + 1, 4001)
+
+	new_cond = cond.copy()
+	new_cond[rule.attr] = range_intersection(cond_range, rule_range)
+	return new_cond
+
+def conjoin_cond_with_negation_of_rule(cond: Cond, rule: Rule) -> Cond:
+	cond_range = cond[rule.attr]
+
+	if rule.op == Operator.LT:
+		rule_range = range(rule.val, 4001)
+	elif rule.op == Operator.GT:
+		rule_range = range(1, rule.val + 1)
+
+	new_cond = cond.copy()
+	new_cond[rule.attr] = range_intersection(cond_range, rule_range)
+	return new_cond
+
+def count_satisfying_cond(cond: Cond) -> int:
+	return prod(len(r) for r in cond.values())
+
+def dst_to_cond_map(workflow: Workflow) -> dict[tuple[str, int], Cond]:
+	"""Returns a dictionary mapping each destination to the condition required
+	for a part to get to that destination when put through the given workflow."""
+	else_cond: Cond = null_cond()
+	result = {}
+
+	for i, rule in enumerate(workflow.rules):
+		cond = conjoin_cond_with_rule(else_cond, rule)
+		result[rule.dst, i] = cond
+		else_cond = conjoin_cond_with_negation_of_rule(else_cond, rule)
+
+	result[(workflow.final_dst, len(workflow.rules))] = else_cond
+	return result
+
+def dstlist_to_cond_map(workflows: dict[str, Workflow]) -> dict[list[tuple[str, int]], Cond]:
+	result = {(('in', -1),): null_cond()}
+
+	while True:
+		did_something = False
+
+		print('---')
+		print(result)
+		print()
+		print(','.join(f'{dst}:{count_satisfying_cond(cond)}' for dst, cond in result.items()))
+		print(sum(count_satisfying_cond(cond) for cond in result.values()))
+		print('---')
+
+		for dstlist in list(result.keys()):
+			if dstlist[-1][0] not in ('A', 'R'):
+				cond_for_dstlist = result.pop(dstlist)
+				submap = dst_to_cond_map(workflows[dstlist[-1][0]])
+				print('WK: ', dstlist[-1], '####', submap)
+
+				for nextdst, nextdst_cond in submap.items():
+					result[dstlist + (nextdst,)] = conjoin_conds(cond_for_dstlist, nextdst_cond)
+
+				did_something = True
+
+		if not did_something:
+			return result
+
+def cond_is_empty(cond: Cond) -> bool:
+	return any(not r for r in cond.values())
+
+def p2(ip: str) -> int:
+	workflows, _ = parse(ip)
+	m = dstlist_to_cond_map(workflows)
+	k = 0
+	k2 = 0
+	print(joinlines(' '.join(map(str, dstlist)) + ' : ' + str(cond) for dstlist, cond in m.items()))
+
+	for dstlist1, cond1 in m.items():
+		for dstlist2, cond2 in m.items():
+			if dstlist1 != dstlist2:
+				assert cond_is_empty(conjoin_conds(cond1, cond2))
+
+	for dstlist, cond in m.items():
+		if dstlist[-1][0] == 'A':
+			k += count_satisfying_cond(cond)
+		elif dstlist[-1][0] == 'R':
+			k2 += count_satisfying_cond(cond)
+		else:
+			breakpoint()
+
+	print(k, k2, k + k2, 4000**4)
+
+	return k
