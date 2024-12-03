53c280cbabd7ba008b8944404c395215ed4ba764
Andrew Foote <footeandrew1@gmail.com>
2024-12-03 11:22:49 +0000

    d3 p1 complete

--- solutions/python/y2024/d3.py
+++ solutions/python/y2024/d3.py
@@ -1,11 +1,13 @@
-from typing import Iterator
+import re
 
 test_inputs = [
     ('example', '''\
-''', [
-        ('p1', 0),
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))''', [
+        ('p1', 161),
     ])
 ]
 
 def p1(ip: str) -> int:
-    return 0
+    return sum(
+        int(a) * int(b) for a, b in re.findall(r'mul\((\d+),(\d+)\)', ip)
+    )
