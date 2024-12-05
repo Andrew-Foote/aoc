0984b8e48a43f6aeb754ba1d7ca78fd1dcfe41e4
Andrew Foote <footeandrew1@gmail.com>
2024-12-05 19:38:04 +0000

    might as well do part 2 now

--- solutions/python/y2015/d5.py
+++ solutions/python/y2015/d5.py
@@ -7,3 +7,25 @@ def is_nice(s: str) -> bool:
  
 def p1(ip:  str) -> str:
     return sum(1 for s in ip.splitlines() if is_nice(s))
+
+def p2_is_nice_1(s: str) -> bool:
+    seen: set[tuple[str, str]] = set()
+    prev: tuple[str, str] | None = None
+
+    for c1, c2 in zip(s, s[1:]):
+        if (c1, c2) in seen:
+            return True
+
+        if prev is not None:
+            seen.add(prev)
+        
+        prev = c1, c2
+
+def p2_is_nice_2(s: str) -> bool:
+    return any(c1 == c3 for c1, c2, c3 in zip(s, s[1:], s[2:]))
+
+def p2_is_nice(s: str) -> bool:
+    return p2_is_nice_1(s) and p2_is_nice_2(s)
+
+def p2(ip: str) -> str:
+    return sum(1 for s in ip.splitlines() if p2_is_nice(s))
