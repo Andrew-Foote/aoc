9581e41cc4f6758bf6333c54566f99e0d97eeab4
Andrew Foote <footeandrew1@gmail.com>
2024-12-06 13:24:04 +0000

    d6 p2 first attempt (too slow)

--- solutions/python/y2024/d6.py
+++ solutions/python/y2024/d6.py
@@ -13,6 +13,7 @@ test_inputs = [
 #.........
 ......#...''', [
         ('p1', 41),
+        ('p2', 6)
     ]),
 ]
 
@@ -25,10 +26,11 @@ test_inputs = [
 # how many distinct positions will the guard visit
 # before leaving mapped area
 
-def p1(ip: str) -> int:
+Grid = dict[gint, str]
+
+def parse(ip: str) -> tuple[Grid, gint]:
     grid: dict[gint, str] = {}
     guard_pos: gint
-    guard_dir: gint = gint(0, -1)
 
     for y, line in enumerate(ip.splitlines()):
         for x, char in enumerate(line):
@@ -38,6 +40,11 @@ def p1(ip: str) -> int:
             if char == '^':
                 guard_pos = pos
 
+    return grid, guard_pos
+
+def p1(ip: str) -> int:
+    grid, guard_pos = parse(ip)
+    guard_dir = gint(0, -1)
     path: set[gint] = {guard_pos}
 
     while True:
@@ -55,3 +62,50 @@ def p1(ip: str) -> int:
             path.add(guard_pos)
 
     assert False
+
+def p2(ip: str) -> int:
+    grid: dict[gint, str] = {}
+    orig_guard_pos: gint
+    open_pos_set: set[gint] = set()
+
+    for y, line in enumerate(ip.splitlines()):
+        for x, char in enumerate(line):
+            pos = gint(x, y)
+            grid[pos] = char
+
+            if char == '^':
+                orig_guard_pos = pos
+            elif char == '.':
+                open_pos_set.add(pos)
+
+    cycle_pos_set = set()
+
+    for open_pos in open_pos_set:
+        grid[open_pos] = '#'
+
+        guard_pos = orig_guard_pos
+        guard_dir = gint(0, -1)
+        path: set[tuple[gint, gint]] = set()
+
+        while True:
+            if (guard_pos, guard_dir) in path:
+                cycle_pos_set.add(open_pos)
+                break
+            else:
+                path.add((guard_pos, guard_dir))
+
+            new_pos = guard_pos + guard_dir
+
+            if new_pos not in grid:
+                break
+           
+            char = grid[new_pos]
+
+            if char == '#':
+                guard_dir *= gint(0, 1)
+            else:
+                guard_pos += guard_dir
+
+        grid[open_pos] = '.'
+
+    return len(cycle_pos_set)
