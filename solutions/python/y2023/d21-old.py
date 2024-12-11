f12b528745386f6785c7dd273d8e99cb14e08282
Andrew Foote <footeandrew1@gmail.com>
2024-12-11 20:19:07 +0000

    greatly simplify solution for y2023 d21 p1

---
+++ solutions/python/y2023/d21-old.py
@@ -0,0 +1,219 @@
+from scipy import sparse
+import numpy as np
+from solutions.python.lib.gint import gint
+import solutions.python.lib.grid as g
+
+test_inputs = [
+	('example', '''\
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........''', [
+		('one_step', '''\
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#O#....
+.##.OS####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........'''),
+		('two_step', '''\
+...........
+.....###.#.
+.###.##..#.
+..#.#O..#..
+....#.#....
+.##O.O####.
+.##.O#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........'''),
+		('three_step', '''\
+...........
+.....###.#.
+.###.##..#.
+..#.#.O.#..
+...O#O#....
+.##.OS####.
+.##O.#...#.
+....O..##..
+.##.#.####.
+.##..##.##.
+...........'''),
+		('six_step', '''\
+...........
+.....###.#.
+.###.##.O#.
+.O#O#O.O#..
+O.O.#.#.O..
+.##O.O####.
+.##.O#O..#.
+.O.O.O.##..
+.##.#.####.
+.##O.##.##.
+...........'''),
+		('six_step_count', 16),
+		('p1', 23),
+		('ten_step_count', 50),
+		('fifty_step_count', 1594),
+		('hundred_step_count', 6536),
+		('five_hundred_step_count', 167004),
+		('thousand_step_count', 668697),
+		('five_thousand_step_count', 16733044)
+	])
+]
+
+# S = start
+# . or S = garden plots
+# # = rocks
+# elf can only move through plots
+# he needs to get 64 steps - q is how many different plots he could
+# potentially arrive at at the end of those steps?
+
+# in other words what is the set of endpoints of all 64-step paths through the graph
+
+def parse_to_grid(ip: str) -> g.Grid:
+	return g.Grid(ip.splitlines())
+
+def parse_to_graph(grid: g.Grid) -> tuple[set[tuple[gint, gint]], gint]:
+	graph = set()
+
+	for z in grid.rect():
+		if grid[z] == '#':
+			continue
+		elif grid[z] == 'S':
+			start = z
+
+		for d in g.NESW:
+			if z + d in grid.rect() and grid[z + d] != '#':
+				graph.add((z, z + d))
+
+	return graph, start
+	# we want the row/col of the adjmat containing the S point, and we want to know how many entries are nonzero
+
+def node_list(graph: set[tuple[gint, gint]]) -> list[gint]:
+	return sorted({e[0] for e in graph} | {e[1] for e in graph}, key=lambda z: (z.real, z.imag))
+
+def adj_matrix(graph: set[tuple[gint, gint]], start: gint) -> tuple[sparse.csr_array, int]:
+	nodes = node_list(graph)
+	print('NODES COUNT: ', len(nodes))
+	
+	row = []
+	col = []
+	data = []
+
+	node_to_index_map = {n: i for i, n in enumerate(nodes)}
+	start_index = node_to_index_map[start]
+
+	for i, (n1, n2) in enumerate(graph):
+		row.append(node_to_index_map[n1])
+		col.append(node_to_index_map[n2])
+		data.append(1)
+
+	m = sparse.csr_array(
+		(data, (row, col)), shape=(len(nodes),) * 2, dtype=np.ubyte
+	)
+
+	return m, start_index
+
+def step_pic(ip: str, count: int) -> str:
+	grid = parse_to_grid(ip)
+	graph, start = parse_to_graph(grid)
+	nodes = node_list(graph)
+	point_to_index_map = {z: i for i, z in enumerate(nodes)}
+	m, start_index = adj_matrix(graph, start)
+	m = sparse.linalg.matrix_power(m, count)
+
+	def picmaker(z: gint) -> str:
+		if z not in point_to_index_map:
+			assert grid[z] == '#', z
+			return '#'
+		elif m[start_index, point_to_index_map[z]] > 0:
+			assert grid[z] in '.S'
+			return 'O'
+		elif z == start:
+			return 'S'
+		else:
+			assert grid[z] == '.'
+			return '.'
+
+	p = grid.rect().picture(picmaker)
+	print()
+	print(p)
+	print()
+	return p
+
+def step(m: sparse.csr_array, count: int) -> sparse.csr_array:
+	# apparently scipy doesn't have a matrix power function for sparse arrays;
+	# m ** count does it element-wise. so we implement exponentiation by
+	# squaring here
+
+	# ok, latest version of scipy DOES have a sparse.linalg.matrix_power
+	# function, but if we use it, we get the wrong answer! weird
+
+	if count == 0:
+		return sparse.csr_array(sparse.eye(m.shape[0]))
+
+	q, r = divmod(count, 2)
+	m_to_q = step(m, q)
+	result = m_to_q @ m_to_q
+
+	if r:
+		result @= m
+
+	return result
+
+def step_count(ip: str, count: int) -> int:
+	grid = parse_to_grid(ip)
+	graph, start = parse_to_graph(grid)
+	nodes = node_list(graph)
+	point_to_index_map = {z: i for i, z in enumerate(nodes)}
+	m, start_index = adj_matrix(graph, start)
+	m = step(m, count)
+	return sum(1 for j, _ in enumerate(nodes) if m[start_index, j] > 0)
+
+def one_step(ip: str) -> str: return step_pic(ip, 1)
+def two_step(ip: str) -> str: return step_pic(ip, 2)
+def three_step(ip: str) -> str: return step_pic(ip, 3)
+def six_step(ip: str) -> str: return step_pic(ip, 6)
+def six_step_count(ip: str) -> int: return step_count(ip, 6)
+def ten_step_count(ip: str) -> int: return step_count(ip, 10)
+def fifty_step_count(ip: str) -> int: return step_count(ip, 50)
+def hundred_step_count(ip: str) -> int: return step_count(ip, 100)
+def five_hundred_step_count(ip: str) -> int: return step_count(ip, 500)
+def thousand_step_count(ip: str) -> int: return step_count(ip, 1000)
+def five_thousand_step_count(ip: str) -> int: return step_count(ip, 5000)
+
+def p1(ip: str) -> int:
+	return step_count(ip, 64)
+
+def p2(ip: str) -> int:
+	base_grid = parse_to_grid(ip)
+
+	grid = g.DefaultGrid(lambda z: base_grid[
+		gint(z.real % base_grid.width, z.imag % base_grid.height)
+	], base_grid)
+
+
+	# basically it's a "chessboard" pattern
+	# given a number of steps, we may consider the subgrid
+
+	# i think we just need to compute the "reachable set"
+	# this is obtainable by searching the graph and not returning to any visited
+	# nodes and implementing the limit
+	# then once we've got the reachable set we count
+		# well, we'll have all the ones on the "outer" part reachable in N steps
+		# we can check how many nodes on the "inside" are adjacnet
+		# 	 those will be excluded
--- solutions/python/y2023/d21.py
+++ solutions/python/y2023/d21.py
@@ -1,10 +1,12 @@
-from scipy import sparse
-import numpy as np
+from collections.abc import Generator
+from enum import Enum
+from typing import assert_never, Self
 from solutions.python.lib.gint import gint
-import solutions.python.lib.grid as g
+from solutions.python.lib.grid import Grid, NESW
+from solutions.python.lib.graph import gbfs
 
 test_inputs = [
-	('example', '''\
+    ('example', '''\
 ...........
 .....###.#.
 .###.##..#.
@@ -16,170 +18,77 @@ test_inputs = [
 .##.#.####.
 .##..##.##.
 ...........''', [
-		('one_step', '''\
-...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#O#....
-.##.OS####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........'''),
-		('two_step', '''\
-...........
-.....###.#.
-.###.##..#.
-..#.#O..#..
-....#.#....
-.##O.O####.
-.##.O#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........'''),
-		('three_step', '''\
-...........
-.....###.#.
-.###.##..#.
-..#.#.O.#..
-...O#O#....
-.##.OS####.
-.##O.#...#.
-....O..##..
-.##.#.####.
-.##..##.##.
-...........'''),
-		('six_step', '''\
-...........
-.....###.#.
-.###.##.O#.
-.O#O#O.O#..
-O.O.#.#.O..
-.##O.O####.
-.##.O#O..#.
-.O.O.O.##..
-.##.#.####.
-.##O.##.##.
-...........'''),
-		('six_step_count', 16),
-		('p1', 23),
-		('ten_step_count', 50),
-		('fifty_step_count', 1594),
-		('hundred_step_count', 6536),
-		('five_hundred_step_count', 167004),
-		('thousand_step_count', 668697),
-		('five_thousand_step_count', 16733044)
-	])
+        ('reachable_count_6_steps', 16),
+    ])
 ]
 
-# S = start
-# . or S = garden plots
-# # = rocks
-# elf can only move through plots
-# he needs to get 64 steps - q is how many different plots he could
-# potentially arrive at at the end of those steps?
+class Cell(Enum):
+    PLOT = '.'
+    ROCK = '#'
 
-# in other words what is the set of endpoints of all 64-step paths through the graph
+    @classmethod
+    def parse(cls, char: str) -> Self:
+        return cls(char)
+    
+    @property
+    def char(self) -> str:
+        return self.value
+    
+def parse(ip: str) -> tuple[Grid[Cell], gint]:
+    start: gint
 
-def parse_to_grid(ip: str) -> g.Grid:
-	return g.Grid(ip.splitlines())
+    def parse_cell(p: gint, char: str) -> Cell:
+        nonlocal start
 
-def parse_to_graph(grid: g.Grid) -> tuple[set[tuple[gint, gint]], gint]:
-	graph = set()
+        match char:
+            case 'S':
+                start = p
+                return Cell.PLOT
+            case '.':
+                return Cell.PLOT
+            case '#':
+                return Cell.ROCK
+            case _:
+                raise ValueError(f"unexpected character '{char}'")
 
-	for z in grid.rect():
-		if grid[z] == '#':
-			continue
-		elif grid[z] == 'S':
-			start = z
+    return Grid.parse(ip.splitlines(), parse_cell), start
 
-		for d in g.NESW:
-			if z + d in grid.rect() and grid[z + d] != '#':
-				graph.add((z, z + d))
+def reachable_count(grid: Grid, start: gint, steps: int) -> int:
+    def children(node: tuple[gint, int]) -> Generator[tuple[gint, int]]:
+        point, step_count = node
 
-	return graph, start
-	# we want the row/col of the adjmat containing the S point, and we want to know how many entries are nonzero
+        if step_count >= steps:
+            return
 
-def node_list(graph: set[tuple[gint, gint]]) -> list[gint]:
-	return sorted({e[0] for e in graph} | {e[1] for e in graph}, key=lambda z: (z.real, z.imag))
+        for d in NESW:
+            neighbour = point + d
 
-def adj_matrix(graph: set[tuple[gint, gint]], start: gint) -> tuple[sparse.csr_array, int]:
-	nodes = node_list(graph)
-	print('NODES COUNT: ', len(nodes))
-	
-	row = []
-	col = []
-	data = []
+            try:
+                cell = grid[neighbour]
+            except KeyError:
+                continue
 
-	node_to_index_map = {n: i for i, n in enumerate(nodes)}
-	start_index = node_to_index_map[start]
+            match cell:
+                case Cell.PLOT:
+                    yield neighbour, step_count + 1
+                case Cell.ROCK:
+                    continue
+                case _:
+                    assert_never(cell)
 
-	for i, (n1, n2) in enumerate(graph):
-		row.append(node_to_index_map[n1])
-		col.append(node_to_index_map[n2])
-		data.append(1)
+    count = 0
+    search = gbfs((start, 0), children)
 
-	m = sparse.csr_array(
-		(data, (row, col)), shape=(len(nodes),) * 2, dtype=np.ubyte
-	)
+    for point, step_count in search:
+        if step_count >= steps:
+            count += 1
 
-	return m, start_index
+    return count
 
-def step_pic(ip: str, count: int) -> str:
-	grid = parse_to_grid(ip)
-	graph, start = parse_to_graph(grid)
-	nodes = node_list(graph)
-	point_to_index_map = {z: i for i, z in enumerate(nodes)}
-	m, start_index = adj_matrix(graph, start)
-	m = sparse.linalg.matrix_power(m, count)
-
-	def picmaker(z: gint) -> str:
-		if z not in point_to_index_map:
-			assert grid[z] == '#', z
-			return '#'
-		elif m[start_index, point_to_index_map[z]] > 0:
-			assert grid[z] in '.S'
-			return 'O'
-		elif z == start:
-			return 'S'
-		else:
-			assert grid[z] == '.'
-			return '.'
-
-	p = grid.rect().picture(picmaker)
-	print()
-	print(p)
-	print()
-	return p
-
-def step_count(ip: str, count: int) -> int:
-	grid = parse_to_grid(ip)
-	graph, start = parse_to_graph(grid)
-	nodes = node_list(graph)
-	point_to_index_map = {z: i for i, z in enumerate(nodes)}
-	m, start_index = adj_matrix(graph, start)
-	m = step(m, count)
-	return sum(1 for j, _ in enumerate(nodes) if m[start_index, j] > 0)
-
-def one_step(ip: str) -> str: return step_pic(ip, 1)
-def two_step(ip: str) -> str: return step_pic(ip, 2)
-def three_step(ip: str) -> str: return step_pic(ip, 3)
-def six_step(ip: str) -> str: return step_pic(ip, 6)
-def six_step_count(ip: str) -> int: return step_count(ip, 6)
+def reachable_count_6_steps(ip: str) -> int:
+    grid, start = parse(ip)
+    return reachable_count(grid, start, 6)
 
 def p1(ip: str) -> int:
-	return step_count(ip, 64)
-
-def p2(ip: str) -> int:
-	base_grid = parse_to_grid(ip)
-
-	grid = g.DefaultGrid(lambda z: base_grid[
-		gint(z.real % base_grid.width, z.imag % base_grid.height)
-	], base_grid)
-
-
-	# basically it's a "chessboard" pattern
-	# given a number of steps, we may consider the subgrid
+    grid, start = parse(ip)
+    return reachable_count(grid, start, 64)
