20627bc38d525d7aa246091dcf292d5f2c97041b
Andrew Foote <footeandrew1@gmail.com>
2024-12-13 13:31:47 +0000

    start d13

---
+++ solutions/python/y2024/d13.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from collections.abc import Generator
+from fractions import Fraction
+import functools as ft
+from pathlib import Path
+import pickle
+from solutions.python.lib.gint import gint
+from solutions.python.lib.grid import Grid, NESW, NE_SE_SW_NW, Rect
+
+test_inputs = [('example', '''\
+''', [
+    ('p1', 0),
+    ('p2', 0),
+])]
+
+def parse(ip: str) -> int:
+    return 0
+
+def p1(ip: str) -> int:
+    return 0
+
+def p2(ip: str) -> int:
+    return 0
