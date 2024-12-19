1c6550deb7f03532d7fbf503518c35b0a72f401f
Andrew Foote <footeandrew1@gmail.com>
2024-12-19 21:48:15 +0000

    d19 part 2 solved (with some help from reddit)

--- solutions/python/y2024/d19.py
+++ solutions/python/y2024/d19.py
@@ -1,8 +1,6 @@
 from enum import Enum
 import functools as ft
 
-import line_profiler
-
 test_inputs = [('example', '''\
 r, wr, b, g, bwu, rb, gb, br
 
@@ -14,17 +12,17 @@ ubwu
 bwurrg
 brgr
 bbrgwb''', [
-#     ('decompositions_csv', '''\
-# b,r,wr,r br,wr,r
-# b,g,g,r
-# g,b,b,r g,b,br gb,b,r gb,br
-# r,r,b,g,b,r r,r,b,g,br r,r,b,gb,r r,rb,g,b,r r,rb,g,br r,rb,gb,r
+    ('decompositions_csv', '''\
+b,r,wr,r br,wr,r
+b,g,g,r
+g,b,b,r g,b,br gb,b,r gb,br
+r,r,b,g,b,r r,r,b,g,br r,r,b,gb,r r,rb,g,b,r r,rb,g,br r,rb,gb,r
 
-# bwu,r,r,g
-# b,r,g,r br,g,r
-# '''),
-#     ('p1', 6),
-#     ('p2', 16),
+bwu,r,r,g
+b,r,g,r br,g,r
+'''),
+    ('p1', 6),
+    ('p2', 16),
 ])]
 
 class Colour(Enum):
@@ -48,50 +46,14 @@ def parse(ip: str) -> tuple[
     designs = [parse_colour_list(s.strip()) for s in designs_s.splitlines()]
     return patterns, designs
 
-def decomp_count(
-    design: tuple[Colour, ...], patterns: set[tuple[Colour, ...]]
-) -> int:
-
-    print(f'DESIGN {format_colour_list(design)}')
-
-    @ft.cache
-    def recurse(design: tuple[Colour, ...]) -> int:
-        if design:
-            result = 0
-
-            if design in patterns:
-                print(f'    {format_colour_list(design)} in pattersn')
-                result += 1
-
-            for i in range(1, len(design)):
-                # the + double-counts decomps in cases like
-                # rgr -> 
-                #        rg, r -> r, g, r
-                #        r, gr -> r, g, r
-                # where r, g, r is counted twice
-                result += recurse(design[:i]) * recurse(design[i:])
-        else:
-            result = 1
-
-        print(f'   recurse({format_colour_list(design)}) = {result}')
-        return result
-    
-    return recurse(design)
-
 def decompositions(
     design: tuple[Colour, ...], patterns: set[tuple[Colour, ...]]
 ) -> set[tuple[tuple[Colour, ...], ...]]:
 
-    # print(f'DESIGN {format_colour_list(design)}')
-    # input()
-
-    @ft.lru_cache(maxsize=32768)
-    @line_profiler.profile
+    @ft.cache
     def recurse(
         design: tuple[Colour, ...]
     ) -> set[tuple[tuple[Colour, ...], ...]]:
-
-        # print(f'   recurse({format_colour_list(design)})')
 
         if not design:
             return set()
@@ -121,13 +83,9 @@ def decompositions(
 def is_possible(
     design: tuple[Colour, ...], patterns: set[tuple[Colour, ...]]
 ) -> bool:
-    
-    # input()
-    # print(f'DESIGN {format_colour_list(design)}')
 
     @ft.cache
     def recurse(design: tuple[Colour, ...]) -> bool:
-        # print(f'   recurse({format_colour_list(design)})')
         if not design:
             return True
         
@@ -160,11 +118,6 @@ def decompositions_csv(ip: int) -> str:
     return '\n'.join(lines)
 
 def p1(ip: str) -> int:
-    # we need to know how many designs are possible
-    # a design is possible if it can be produced by concatenation of
-    # available patterns (where a pattern can occur any number of times,
-    # provided it's available)
-
     patterns, designs = parse(ip)
     result = 0
 
@@ -174,13 +127,38 @@ def p1(ip: str) -> int:
 
     return result
 
-def p2(ip: str) -> int:
+def decomp_count(
+    design: tuple[Colour, ...], patterns: set[tuple[Colour, ...]]
+) -> int:
 
+    @ft.cache
+    def recurse(design: tuple[Colour, ...]) -> int:
+        # any decomposition must begin with one of the patterns
+        # and if we chop that pattern off, what remains will be a decomposition
+        # of the design with that pattern chopped off
+        # so the count of decompositions that start with that pattern will be
+        # equal to the count of the chopped-off design
+        # furthermore, there's no overlap between decompositions from different
+        # chopped-off patterns because they all start with a different pattern
+
+        if not design:
+            return 1
+        
+        result = 0
+
+        for pattern in patterns:
+            if len(pattern) <= len(design) and design[:len(pattern)] == pattern:
+                result += recurse(design[len(pattern):])
+
+        return result
+    
+    return recurse(design)
+
+def p2(ip: str) -> int:
     patterns, designs = parse(ip)
     result = 0
 
     for design in designs:
-        # result += decomp_count(design, patterns)
-        result += len(decompositions(design, patterns))
+        result += decomp_count(design, patterns)
 
     return result
