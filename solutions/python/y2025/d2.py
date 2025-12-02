bccf69a31adb0701fd5497f985e8d1b24248131d
Andrew Foote <footeandrew1@gmail.com>
2025-12-02 16:32:16 +0000

    more efficient, mathy solution for d2

--- solutions/python/y2025/d2.py
+++ solutions/python/y2025/d2.py
@@ -1,10 +1,13 @@
 from collections.abc import Iterator
+import math 
+from solutions.python.lib.digits import digit_count
 
 test_inputs = [
     ('example', '''\
 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
 824824821-824824827,2121212118-2121212124''', [
+        ('p1_invalid_ids_csv', '11,22;99;1010;1188511885;222222;;446446;38593859;;;'),
         ('p1', 1227775554),
         ('p2_invalid_ids_csv', '11,22;99,111;999,1010;1188511885;222222;;446446;38593859;565656;824824824;2121212121'),
         ('p2', 4174379265)
@@ -18,12 +21,14 @@ def parse(ip: str) -> Iterator[tuple[int, int]]:
         a, b = r.split('-')
         yield int(a), int(b)
 
-# An ID is invalid if it is of the form
+# The invalid IDs are the positive integers with decimal expansions of the form
 # 
-#   d_(n - 1) ... d_0 d_(n - 1) ... d_0
+#   d_(n - 1) ... d_0 d_(n - 1) ... d_0,
 #
-# for some positive integer n. If we interpret this as a decimal expansion then
-# the number represented by it is
+# where n is a positive integer.
+# 
+# Any such integer has an even number of digits (namely 2n). It can also be 
+# written as
 # 
 #   d_(n - 1) * 10^(2n - 1) + ... + d_0 * 10^n 
 #     + d_(n - 1) * 10^(n - 1) + ... + d_0
@@ -32,144 +37,97 @@ def parse(ip: str) -> Iterator[tuple[int, int]]:
 #   = r * 10^n + r
 #   = r(10^n + 1),
 #
-# where r is the number with decimal expansion d_(n - 1) ... d_0. And since IDs
-# can't have leading zeros, we have d_(n - 1) != 0 meaning a has exactly n 
-# digits, i.e. 10^(n - 1) <= a < 10^n.
-#
-# Conversely, for any two positive integers a and n, if 10^(n - 1) <= a < 10^n,
-# then a(10^n + 1) = a * 10^n + a, and the leading digit of a is nonzero, so 
-# the decimal expansion of a(10^n + 1) will be an invalid ID.
-#
-# Our problem is to find the invalid IDs within a given range. In other words,
-# given positive integers a, b, what are the integers x such that a <= x <= b,
-# and there are positive integers n with 10^n + 1 | x and 10^(n - 1) < x < 10^n?
-# Well, any such x will have 10^n + 1 <= x which
-#
-# Well, if q(10^n + 1) <= b then 
+# where r is the positive integer with decimal expansion d_(n - 1) ... d_0. Thus
+# it is divisible by 10^n + 1. Note that r has exactly n digits.
+# 
+# Conversely, for any positive integer x, if there are positive integers n and
+# r such that r has n digits and x = r(10^n + 1), then x is an invalid ID (with
+# 2n digits).
+# 
+# The problem is to find all the invalid IDs within a given inclusive range
+# [a, b]. We could just check each integer within the range, but it will be
+# more efficient to consider the range of possible numbers of digits of r. Since
+# the ID will have twice as many digits as r, we'll have A <= 2r <= B, where
+# where A is the number of digits of a, and B is the number of digits of b.
+# So the possible numbers of digits of r are the elements of
+# [ceil(A/2), floor(B/2)]. Given an n within this range, the possible values of
+# r will be all multiples of y = 10^n + 1 that do indeed have n digits. To find
+# these we can search through the range [ceil(a/y), floor(b/y)], checking for 
+# each member whether it does indeed have n digits.
 
-def is_valid(pid: str) -> bool:
-    if len(pid) % 2:
-        return True
-    
-    n = len(pid) // 2
+def p1_invalid_ids(ip: str) -> Iterator[list[int]]:
+    for a, b in parse(ip):
+        A = digit_count(a)
+        B = digit_count(b)
 
-    return int(pid) % (10 ** n + 1)
+        pids = set()
 
-    return pid[:n] != pid[n:]
+        for n in range(math.ceil(A/2), math.floor(B/2) + 1):
+            y = 10 ** n + 1
+                
+            for r in range(math.ceil(a/y), math.floor(b/y) + 1):
+                if digit_count(r) == n:
+                    pids.add(r * y)
+
+        yield sorted(pids)
+
+def p1_invalid_ids_csv(ip: str) -> str:
+    return ';'.join(','.join(map(str, pids)) for pids in p1_invalid_ids(ip))
 
 def p1(ip: str) -> int:
-    s = 0
-
-    for a, b in parse(ip):
-        for x in range(a, b + 1):
-            if not is_valid(str(x)):
-                s += x
-
-    return s
-
-def is_valid_p2(pid: str) -> bool:
-    pids_to_debug = ()
-    debug = lambda s: print(s) if pid in pids_to_debug else None 
-    debug(f'CHECKING WHETHER {pid} IS VALID')
-
-    for part_count in range(2, len(pid) + 1):
-        debug(f'  {part_count=}')
-
-        if len(pid) % part_count:
-            debug(f' cannae be split evenly for this part count')
-            continue
-        
-        part_size = len(pid) // part_count
-        debug(f'  {part_size=}')
-    
-        x = 10 ** (part_count * part_size) - 1
-        y = 10 ** part_size - 1
-
-        if int(pid) % (x // y) == 0:
-            return False
-
-    return True
-
-def p2_invalid_ids_old(ip: str) -> Iterator[list[int]]:
-    for a, b in parse(ip):
-        pids = []
-
-        for x in range(a, b + 1):
-            if not is_valid_p2(str(x)):
-                pids.append(x)
-
-        yield pids
+    return sum(sum(pids) for pids in p1_invalid_ids(ip))
 
 def p2_invalid_ids_csv(ip: str) -> str:
     return ';'.join(','.join(map(str, pids)) for pids in p2_invalid_ids(ip))
 
-# An ID is invalid if there is a sequence of digits d_(n - 1), ..., d_0 such
-# that the ID is just that sequence repeated m times, for some integer m >= 2.
-# If this is the case, then letting a be the number represented by
-# d_(n - 1) ... d_0, so that
-#
-#   a = d_(n - 1) * 10^(n - 1) + ... + d_1 * 10 + d_0,
-#
-# then the invalid ID will represent the number
-#
-#   a * 10^((m - 1)n) + ... + a * 10^n + a
-#   = a((10^n)^(m - 1) + ... + 10^n + 1)
-#   = a((10^n)^m - 1)/(10^n - 1)
-#   = a(10^mn - 1)/(10^n - 1).
-#
-# Conversely, any number of the form a(10^mn - 1)/(10^n - 1), where m is an
-# integer >= 2, n is a positive integer, and a is positive integer with exactly 
-# n digits, corresponds to an invalid ID. (The "exactly n digits" condition is 
-# necessary because IDs can't have leading zeros.)
-#
-# So our problem is to find all numbers of this form within a given inclusive
-# range [A, B]. Since a number of this form can also be written as 
-#
-#   a * 10^((m - 1)n) + ... + a * 10^n + a,
-#
-# it is >= a * 10^((m - 1)n), and since a has exactly n digits, so that
-# a >= 10^(n - 1), it follows that the number is >= 10^(mn - 1). So if the
-# number is <= B it follows that 10^(mn - 1) <= B and hence mn - 1 <= log_10 B.
-# So we can limit our search to pairs (m, n) with mn <= log_10 B + 1. Given
-# such a pair, we can calculate x = 10^mn - 1, y = 10^n - 1, and then we want
-# to find a with A <= ax/y <= B, i.e. Ay <= ax <= By, i.e.
-#  ceil(Ay/x) <= a <= floor(By/x). But we also want to ensure ax/y is an 
-# integer...
-#  Ay <= ax <= By
-#  ceil(Ay) <= ax <= 
+# For part 2, the invalid IDs are the positive integers with decimal expansions
+# consisting of a block of digits d_(n - 1), ..., d_0 repeated m times, for 
+# some integer m >= 2.
 # 
-#  x = (10^mn - 1)/(10^n - 1) and then just take
-# all the multiples of x within [A, B] that are integers
-# A <= kx <= B
-# A/x <= k <= B/x
-
-import math 
+# Any such integer has a number of digits divisible by m (namely mn). It can
+# also be written as
+#
+#   r * 10^((m - 1)n) + ... + r * 10^n + a
+#   = r((10^n)^(m - 1) + ... + 10^n + 1)
+#   = r((10^n)^m - 1)/(10^n - 1)
+#   = r(10^mn - 1)/(10^n - 1),
+#
+# where r is the positive integer with decimal expansion d_(n - 1) ... d_0. Thus
+# it is divisible by (10^mn - 1)/(10^n - 1).
+#
+# Conversely, for any positive integer x, if there are positive integers m and n
+# such that m >= 2, r has n digits and x = a(10^mn - 1)/(10^n - 1), then x is an
+# invalid ID (within mn digits).
+# 
+# The problem is to find all the invalid IDs within a given inclusive range
+# [a, b]. We could just check each integer within the range, but it will be
+# more efficient to consider the range of possible numbers of digits of r. Given
+# a choice of m and n, the ID itself will have mn digits, and since it will
+# belong to [a, b] we must have A <= mn <= B where A is the number of digits of 
+# a, and B is the number of digits of b. This implies m <= B, and we also know
+# that 2 <= m so we'll have 2 <= m <= B. So we can just search through the range
+# [2, B] for possible values of m. Then, for a given choice of m, n must
+# satisfy A/m <= n <= B/m, so we search through [ceil(A/m), floor(B/m)]. Then,
+# given m and n, the possible values of r are all multiples of 
+# y = (10^mn - 1)/(10^n - 1) that have n digits. To find these we can search
+# through the range [ceil(a/y), floor(b/y)].
 
 def p2_invalid_ids(ip: str) -> Iterator[list[int]]:
     for a, b in parse(ip):
-        pids = []
-        pid_set = set()
+        A = digit_count(a)
+        B = digit_count(b)
 
-        for p in range(0, math.floor(math.log10(b) + 1) + 1):
-            for m in range(2, p + 1):
-                n = p // m
-                x = 10 ** (m * n) - 1
-                y = 10 ** n - 1
+        pids = set()
 
-                for k in range(math.ceil(a * y / x), math.floor(b * y / x) + 1):
-                    if 10 ** (n - 1) <= k < 10 ** n and (k * x) % y == 0:
-                        pid = int(k * x / y)
+        for m in range(2, B + 1):
+            for n in range(math.ceil(A/m), math.floor(B/m) + 1):
+                y = (10 ** (m * n) - 1) // (10 ** n - 1)
+                
+                for r in range(math.ceil(a/y), math.floor(b/y) + 1):
+                    if digit_count(r) == n:
+                        pids.add(r * y)
 
-                        if pid not in pid_set:
-                            pid_set.add(pid)
-                            pids.append(pid)
-
-        yield pids
+        yield sorted(pids)
 
 def p2(ip: str) -> int:
-    s = 0
-
-    for pids in p2_invalid_ids(ip):
-        s += sum(pids)
-    
-    return s
+    return sum(sum(pids) for pids in p2_invalid_ids(ip))
