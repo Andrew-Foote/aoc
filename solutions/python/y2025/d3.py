9fe3600df7962d84ceebd1125945dff9adf6d437
Andrew Foote <footeandrew1@gmail.com>
2025-12-03 12:05:09 +0000

    brute force solution for p2 which is too slow

--- solutions/python/y2025/d3.py
+++ solutions/python/y2025/d3.py
@@ -1,4 +1,6 @@
 from collections.abc import Iterator
+from solutions.python.lib.digits import int_from_digits_leading_first as fromdigits
+import itertools as it
 
 test_inputs = [
     ('example', '''\
@@ -6,7 +8,9 @@ test_inputs = [
 811111111111119
 234234234234278
 818181911112111''', [
-        ('p1', 357)
+        ('p1', 357),
+        ('p2_largest_joltages_csv', '987654321111,811111111119,434234234278,888911112111'),
+        ('p2', 3121910778619),
     ])
 ]
 
@@ -30,3 +34,32 @@ def largest_joltages(ip: str) -> Iterator[int]:
 
 def p1(ip: str) -> int:
     return sum(largest_joltages(ip))
+
+def possible_index_lists(length: int, n: int, cur: list[int]) -> Iterator[list[int]]:
+    if len(cur) == n:
+        yield cur
+        return
+
+    start = cur[-1] + 1 if cur else 0
+
+    for i in range(start, length):
+        yield from possible_index_lists(length, n, cur + [i])
+
+def possible_joltages_p2(bank: list[int], n: int=12) -> Iterator[int]:
+    for indices in possible_index_lists(len(bank), n, []):
+        ds = [bank[i] for i in indices]
+        yield fromdigits(ds)
+
+def largest_joltage_p2(bank: list[int]) -> int:
+    return max(possible_joltages_p2(bank))
+
+def largest_joltages_p2(ip: str) -> Iterator[int]:
+    for bank in parse(ip):
+        print(f'checking bank {bank}')
+        yield largest_joltage_p2(bank)
+
+def p2_largest_joltages_csv(ip: str) -> str:
+    return ','.join(map(str, largest_joltages_p2(ip)))
+
+def p2(ip: str) -> int:
+    return sum(largest_joltages_p2(ip))
