13016d4a1e7fe94cb0a648eedb2151f4324ef686
Andrew Foote <footeandrew1@gmail.com>
2025-12-04 13:21:47 +0000

    automatically run mypy

--- methods/python.py
+++ methods/python.py
@@ -1,7 +1,20 @@
 import functools as ft
 import importlib
 import methodlib
+import mypy.api
 from typing import Any
+
+def _module_path(year, day):
+    alt = '_alt' if year == 2022 and day == 19 else ''
+    return f'solutions/python/y{year}/d{day}{alt}.py'
+
+@ft.cache
+def _typecheck(year, day) -> None:
+    mypy_stdout, mypy_stderr, mypy_ret = mypy.api.run([_module_path(year, day)])
+
+    if mypy_ret != 0:
+        print(f'\n\n{mypy_stdout}')
+        raise Exception('typecheck failed')
 
 @ft.cache
 def _module(year, day):
@@ -12,6 +25,7 @@ def has_facet(year: int, day: int, facet: str):
     return hasattr(_module(year, day), facet)
 
 def run_facet(year: int, day: int, facet: str, ip: str) -> str:
+    _typecheck(year, day)
     return str(getattr(_module(year, day), facet)(ip))
 
 def test_defs(year: int, day: int) -> methodlib.TestDefs:
